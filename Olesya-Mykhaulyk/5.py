123c58fab64ab8f860d7119ca40adcccc422ae0b
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-02-28 22:56:52 +0200

    Задачі до розділу 5 (варіант 10_ перші 3)

---
+++ Olesya-Mykhaulyk/5.py
@@ -0,0 +1,8 @@
+#Olesya Mykhaulyk ALs-12 5.10 - 5
+d={} # stvoryemo pystui slovnuk
+d['apple']=['N'] # dodaemo v slovnuk slova
+d['drive']=['V']
+d['pretty']=['Adj']
+d['monkey']=['N']
+d.keys() # peregladaemo spusok kly4iv slovnuka
+d['xyz']# probyemo dostyputusa do kly4a xyz, vubuvae pomulky dostypy,oskilku takui kly4 vidsytnii:
