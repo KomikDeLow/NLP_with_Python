1c1ec3ff75591288ec2571b02b83005994c6fbdd
Iryna <ira-shtanhret@mail.ru>
2013-02-28 19:52:24 +0200

    Update Task 7.py

--- IrynaShtanhret/Task 7.py
+++ IrynaShtanhret/Task 7.py
@@ -9,3 +9,19 @@ d1.update(d2)#�� ��������� ������� update �
 sorted(d1)
 ['be', 'girl', 'like', 'summer', 'tiger', 'want', 'weather', 'winter']#������� 1 ���������� �����
 
+#Штангрет Ірина, ПРЛс-13, завдання 7
+import nltk
+ from nltk import*
+d1={}.fromkeys(['want', 'be', 'like'])#stvoryla slovnyk 1
+d2={}.fromkeys(['winter', 'summer', 'weather', 'girl', 'tiger'])#stvoryla slovnyk 2
+d1['want']='V'#nadala znachennya kozhnomu slovu
+d1['be']='V'
+d1['like']='V'
+d2['winter']='N'
+d2['summer']='N'
+d2['weather']='N'
+d2['girl']='N'
+d2['tiger']='N'
+d1.update(d2)#slovnyk 1 dopovnyvsya 2
+d1
+{'be': 'V', 'like': 'V', 'summer': 'N', 'tiger': 'N', 'weather': 'N', 'want': 'V', 'girl': 'N', 'winter': 'N'}
