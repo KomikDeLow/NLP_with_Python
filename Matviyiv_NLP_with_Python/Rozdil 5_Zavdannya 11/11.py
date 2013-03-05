a784b140bf09442adfce94683e11a59fed1b02df
Michael Matviyiv <mriynuk99@gmail.com>
2013-03-05 10:00:44 +0200

    Matviyiv_NLP

---
+++ Matviyiv_NLP_with_Python/Rozdil 5_Zavdannya 11/11.py
@@ -0,0 +1,40 @@
+import nltk
+from nltk.corpus import brown
+brown_tagged_sents = brown.tagged_sents(categories='news')
+aff_tagger=nltk.AffixTagger(brown_tagged_sents)
+print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
+                      'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
+                      'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
+                      'that', 'students', 'of', 'pre-intermediate', 'level', 'can', 'easily',
+                      'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
+print '==============================================================='
+print 'Affix tagger size = ',aff_tagger.size()
+print 'Sentences used to evaluete = ',len(brown_tagged_sents)
+print 'Evaluation = ',aff_tagger.evaluate(brown_tagged_sents)
+print '==============================================================='
+
+brown_tagged_sents = brown.tagged_sents(categories='news')
+aff_tagger=nltk.AffixTagger(brown_tagged_sents,affix_length=-2)
+print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
+                      'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
+                      'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
+                      'that', 'students', 'of', 'pre-intermediate', 'level', 'can', 'easily',
+                      'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
+print '==============================================================='
+print 'Affix length changed to = -2 ','Affix tagger size = ',aff_tagger.size()
+print 'Sentences used to evaluete = ',len(brown_tagged_sents)
+print 'Evaluation = ',aff_tagger.evaluate(brown_tagged_sents)
+print '==============================================================='
+
+brown_tagged_sents = brown.tagged_sents(categories='news')
+aff_tagger=nltk.AffixTagger(brown_tagged_sents,min_stem_length=4)
+print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
+                      'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
+                      'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
+                      'that', 'students', 'of', 'pre-intermediate', 'level', 'can', 'easily',
+                      'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
+print '==============================================================='
+print 'Minimum stem length changed to = 4 ','Affix tagger size = ',aff_tagger.size()
+print 'Sentences used to evaluete = ',len(brown_tagged_sents)
+print 'Evaluation = ',aff_tagger.evaluate(brown_tagged_sents)
+print '==============================================================='
---
+++ Matviyiv_NLP_with_Python/Rozdil 5_Zavdannya 6/6.py
@@ -0,0 +1,10 @@
+dict1={}
+dict1['colorless'] = 'ADJ'
+dict1['ideas'] = 'N'
+dict1['sleep'] = 'V'
+dict1['furiously'] = 'ADV'
+print 'Dictionary========================'
+print dict1
+del dict1['sleep']
+print 'Deleted entry (sleep)'
+print dict1
