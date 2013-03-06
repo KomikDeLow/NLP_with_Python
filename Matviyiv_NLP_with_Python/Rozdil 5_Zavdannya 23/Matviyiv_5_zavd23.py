7d642843b4df375e1b8ec8f3637a20dbfaffb9d9
Michael Matviyiv <mriynuk99@gmail.com>
2013-03-06 18:51:26 +0200

    Matviyiv_23_and_30

---
+++ Matviyiv_NLP_with_Python/Rozdil 5_Zavdannya 23/Matviyiv_5_zavd23.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.corpus import brown
+patterns = [
+    (r'.*ing$', 'VBG'), # gerunds
+    (r'.*ed$', 'VBD'), # simple past
+    (r'.*es$', 'VBZ'), # 3rd singular present
+    (r'.*ould$', 'MD'), # modals
+    (r'.*\'s$', 'NN$'), # possessive nouns
+    (r'.*s$', 'NNS'), # plural nouns
+    (r'^-?[0-9]+(.[0-9]+)?$', 'CD'), # cardinal numbers
+    (r'.*', 'NN') # nouns (default)
+    ]
+regexp_tagger = nltk.RegexpTagger(patterns)
+regexp_tagger.tag(brown.sents()[9])
+brown_test=brown.tagged_sents()[:100]
+print 'Regular expressions Tagger:'
+print 'Accuracy: %4.1f%%' % (
+    100.0 * regexp_tagger.evaluate(brown_test))
+
+
+two_tagger=nltk.UnigramTagger(brown_test, backoff=regexp_tagger)
+print 'Regular expressions Tagger + Unigram tagger:'
+print 'Accuracy: %4.1f%%' % (
+    100.0 * two_tagger.evaluate(brown_test))
