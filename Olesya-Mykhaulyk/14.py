b25d0950a19442bb366d5060f2825d6ce5ad24df
Andriy Romanyuk <anrom7@gmail.com>
2013-03-07 02:27:59 +0200

    TODO added

--- Olesya-Mykhaulyk/14.py
+++ Olesya-Mykhaulyk/14.py
@@ -3,6 +3,8 @@
 #You use a lot variebles (words_brown,tage_brown,tage_brown,sortyem,bez_dyblikativ) and it is a bad programming style
 #You append tags to not empty tage_brown list
 #Olesya Mykhaulyk ALs-12 5.10-14
+#TODO
+#What do your loop do?
 import nltk
 from nltk.corpus import brown
 words_brown=brown.tagged_words()
