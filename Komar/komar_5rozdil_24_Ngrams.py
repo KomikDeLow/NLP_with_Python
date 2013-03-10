b47bf7c60b292ed6e9095b4a470811302680bcf1
Mariia Komar <komar.mariia>
2013-03-10 17:36:06 +0200

    Ex. 24

---
+++ Komar/komar_5rozdil_24_Ngrams.py
@@ -0,0 +1,5 @@
+import nltk
+from nltk.corpus import brown
+for i in range (1,6):
+	n_grams = nltk.NgramTagger (i,nltk.corpus.brown.tagged_sents()[:10000])
+	print n_grams.evaluate(nltk.corpus.brown.tagged_sents()[:10000])
