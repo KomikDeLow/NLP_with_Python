1b0365ce5594fcd64f355768a4ec19a7baa81542
Prokulevych K. <qqqkate@mail.ru>
2013-03-11 15:41:00 +0200

    Chapter5

---
+++ prokulevych-k/Prokulevych_Chapter5_25.py
@@ -0,0 +1,31 @@
+import nltk
+fd= nltk.FreqDist(nltk.corpus.floresta.words ())
+a= fd.keys()[:150]
+a
+print a
+cfd= nltk.ConditionalFreqDist(nltk.corpus.floresta.tagged_words())
+b= dict((word, cfd[word].max()) for word in a)
+b['primeiro']
+print b['primeiro']
+b['sempre']
+print b['sempre']
+b['novo']
+print b['novo']
+b['Se']
+print b['Se']
+unigram_tagger= nltk.UnigramTagger (model=b)
+unigram_tagger.evaluate(nltk.corpus.floresta.tagged_sents())
+print unigram_tagger.evaluate(nltk.corpus.floresta.tagged_sents())
+c= nltk.corpus.floresta.tagged_sents ()
+t= nltk.DefaultTagger('NN')
+t1 = nltk.UnigramTagger(c, backoff=t)
+t2=nltk.BigramTagger(c, cutoff=0, backoff=t1)
+t2.evaluate(c)
+print t2.evaluate(c)
+from nltk.corpus import treebank
+d=nltk.corpus.treebank.tagged_sents()
+t= nltk.DefaultTagger('NN')
+t1 = nltk.UnigramTagger(d, backoff=t)
+t2=nltk.BigramTagger(d, cutoff=0, backoff=t1)
+t2.evaluate(d)
+print t2.evaluate(d)
