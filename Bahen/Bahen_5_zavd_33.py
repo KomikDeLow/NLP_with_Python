b47baefa5c2458aac45faf14cb2d18b380cca0d0
Diana <bahen@ukr.netr>
2013-03-12 20:07:08 +0200

    Chapter_5_33

---
+++ Bahen/Bahen_5_zavd_33.py
@@ -0,0 +1,16 @@
+import nltk
+brown_tagged=nltk.corpus.brown.tagged_words()
+def tag(word,tag):
+    w=dict()
+    a=nltk.defaultdict(dict)
+    c=[]
+    for i in range(len(brown_tagged)-1):
+        d=brown_tagged[i]
+        e=brown_tagged[i+1]
+        if d[0]==word and d[1]==tag:
+            c+=[e[1]]
+    w[tag]=set(c)
+    a[word]=w
+    return a
+tag('moon','NN')
+print tag('moon','NN')
