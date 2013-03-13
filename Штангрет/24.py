68248b8f300d6c9c77af166990f675370f84c812
Христина <Khrystyna.Maschakhevich@gmail.com>
2013-03-13 23:31:41 +0200

    24, 28 Shtanhret Iryna

---
+++ Штангрет/24.py
@@ -0,0 +1,11 @@
+# -*- coding: cp1251 -*-
+#Shtanhret Iryna AL-13, zadacha 24
+import nltk
+from nltk.corpus import brown
+brown_news_tagged=brown.tagged_sents(categories='news', simplify_tags=True)
+test=brown.sents(categories='news')
+for n in range (6):
+	ngram=nltk.NgramTagger(n, brown_news_tagged)
+	ngram.tag(test[3])
+	print ngram.evaluate(brown_news_tagged)#побудувала цикл з н-грам тегів від 1-6 та оцінила через функцію евалуейт результати
+
