d47a34404d6b5e8ac197ffd8576708f0ba1f3dad
Дарія Морозова <dashysik.nirvana@gmail.com>
2013-03-15 00:52:01 +0200

    Розділ 5 задача 6

---
+++ DariaMorozova/r.5.zad.6 var 11.py
@@ -0,0 +1,10 @@
+# Daria Morozova
+# PRLs-12
+# zad.6 
+
+pos = {}
+pos = {'white': 'ADJ', 'dreams': 'N', 'play': 'V', 'angrily': 'ADV'}
+print pos
+del pos['play']
+print 'Deleted entry (play)'
+print pos
