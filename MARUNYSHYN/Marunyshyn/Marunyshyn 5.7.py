07188fc4aa2689c38d6923cd613e8bc0ebfcbb46
Irena Marunyshyn <Irenamarunyshyn@yahoo.com>
2013-03-14 21:19:06 +0200

    5розділ 7 задача

---
+++ MARUNYSHYN/Marunyshyn/Marunyshyn 5.7.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk import*
+d1={}.fromkeys(['call','mother','home'])
+d2={}.fromkeys(['speak','father','big'])
+d1['call']='V'
+d1['mother']='N'
+d1['home']='N'
+d2['speak']='V'
+d2['father']='N'
+d2['big']='A'
+d1.update(d2)
+d1
+print d1
