366483eaabb81ffe9c6811a97f0a154d074040ea
Nataliia Kozachuk <natalyakozatchuk@yahoo.com>
2013-03-14 21:00:29 +0200

    NatalyaKozatchuk 6 var_rozdil5_Ex6

---
+++ kozachuk/nataliia-kozachuk/Kozachyk_rozdil_5_ex_6.py
@@ -0,0 +1,17 @@
+dict1={}
+dict1['meningless']='ADJ'
+#part-of-speech is adjective
+dict1['images']='N'
+#-noun
+dict1['walk']='V'
+#-verb
+dict1['angrily']='ADV'
+#-adverb
+print 'Dictionary========================'
+print dict1
+del dict1['walk']
+#vudaljaemo element walk
+print 'deleted entry (walk)'
+#pokazye resyltat, v jakomy vge vidsytnij 'walk'
+print dict1
+
