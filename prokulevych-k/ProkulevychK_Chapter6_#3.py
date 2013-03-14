fcbe0f8048a2f44d7f81698088a9d0a20bb254bb
Prokulevych K. <qqqkate@mail.ru>
2013-03-14 22:45:43 +0200

    Chapter6_#3

---
+++ prokulevych-k/ProkulevychK_Chapter6_#3.py
@@ -0,0 +1,20 @@
+import nltk
+import types
+from nltk.corpus import senseval
+instances=senseval.instances('line.pos')
+features=[]
+for inst in instances:
+    w=[]
+for i in inst.context:
+	if type(i)==types.TupleType:
+		w.append(i)
+	elif type(i)==types.StringType:
+		w.append(("None",i))
+	a={"word":inst.word,"position":inst.position,}
+	b=dict(w)
+	a.update(b)
+	features.append((a,' '.join(inst.senses)))
+size=int(len(features)*0.1)
+train_set, test_set=features[size:],features[:size]
+classifier=nltk.NaiveBayesClassifier.train(train_set)
+print nltk.classify.accuracy(classifier,test_set)
