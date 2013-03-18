724d63e1252c521c6aee5d16f0cdeae295dc11fc
Halyna Khimka <galinka_khimka@ukr.net>
2013-03-18 12:58:10 +0200

    Task_24_Chapter_5

---
+++ Halyna_Khimka/task_24_Chapter_5_Khimka.py
@@ -0,0 +1,15 @@
+# Halyna Khimka
+# Chapter 5, Task 24
+# How serious is the sparse data problem? Investigate the performance of n-gram
+#taggers as n increases from 1 to 6. Tabulate the accuracy score. Estimate the training
+#data required for these taggers, assuming a vocabulary size of 105 and a tagset size
+#of 102.
+import nltk
+from nltk.corpus import brown
+sents=brown.sents()
+tagged_sents=brown.tagged_sents()
+train_sents=tagged_sents[:1000] # dani dlia trenuvannja
+diap=[1,2,3,4,5,6] # diapazon zna4enj
+for i in diap:
+	t=nltk.NgramTagger(n=i, train=train_sents)
+	print 'N=',i, 'Evaluation:', t.evaluate (tagged_sents[1000:1200])
