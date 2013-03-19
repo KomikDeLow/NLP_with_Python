d30cdfb1ffd5a95e254aa080aea294736121e752
Andriy Romanyuk <anrom7@gmail.com>
2013-03-19 14:24:11 +0200

    TODO added

--- Klantsak_Chapter_5_32.py
+++ Klantsak_Chapter_5_32.py
@@ -1,3 +1,6 @@
+﻿# TODO
+# Коментарі! Що програма робить?
+# Потрібно провести декілька експериментів з різними значеннями аргументів
 import nltk
 help(nltk.tag.brill.demo)
 nltk.tag.brill.demo(num_sents=90, max_rules=100,  min_score=4, trace=7, train=0.9)
