4e7ea145d7569b3d64da0e6f5ed52f9574366c76
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-03-21 21:33:51 +0200

    розділ 5 завдання 24

---
+++ Olesya-Mykhaulyk/24.py
@@ -0,0 +1,11 @@
+#Olesya Mykhaulyk ALs-12
+import nltk
+from nltk.corpus import brown
+brown_sents=brown.sents()
+brown_tagged_sents=brown.tagged_sents() #dostypaemos do re4en
+size=int(len(brown_tagged_sents)*0,9)
+train_sents=brown_tagged_sents[:size] # dani na jakuh provodutsa trenyvanja
+    for n in range(6):
+        ngram=nltk.NgramTagger(n,train=brown_tagged_sents)
+        print n, ngram.evaluate(brown_tagged_sents[size:]) # otsinka robotu i vuvedenja rezyltativ
+
