f9db8dfa8e1b673d461186dfb132a611859562a0
Prokulevych K. <qqqkate@mail.ru>
2013-03-22 16:04:38 +0200

    Prokulevych K.
    Chapter5 #32

--- prokulevych-k/Prokulevych_Chapter5_№32.py
+++ prokulevych-k/Prokulevych_Chapter5_№32.py
@@ -5,4 +5,7 @@
 # performance?
 import nltk
 help(nltk.tag.brill.demo)
-nltk.tag.brill.demo(num_sents=90, max_rules=100,  min_score=4, trace=7, train=0.9)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.4, trace=4)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.6, trace=4)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.9, trace=4)
+#зі збільшенням розміру корпусу для опрацювання уповільнюється процес виконання
