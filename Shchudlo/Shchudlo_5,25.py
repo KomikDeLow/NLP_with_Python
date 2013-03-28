f8ad455775f2605ff4156b4cdf2502c7d674c1a7
Luchunka21 <luchunka21@gmail.com>
2013-03-28 17:33:54 +0200

    done
    divide train and test data

--- Shchudlo/Shchudlo_5,25.py
+++ Shchudlo/Shchudlo_5,25.py
@@ -3,23 +3,35 @@
 #
 import nltk
 from nltk.corpus import *
-Unigram_English=nltk.UnigramTagger(brown.tagged_sents()[:1000])
-Unigram_Brazilian=nltk.UnigramTagger(mac_morpho.tagged_sents()[:1000])
+test_data1=brown.tagged_sents()[:1000]
+train_data1=brown.tagged_sents()[1000:9000]
+
+Braz_test_data1=mac_morpho.tagged_sents()[:1000]
+Braz_train_data1=mac_morpho.tagged_sents()[1000:9000]
+
+Unigram_English=nltk.UnigramTagger(train_data1)
+Unigram_Brazilian=nltk.UnigramTagger(Braz_train_data1)
 
 Unigram_English.tag(brown.sents()[9])
 print 'Accuracy of Unigram tagger in English: %4.1f%%' % (
-    100.0 * Unigram_English.evaluate(brown.tagged_sents()[1000:2000]))
+    100.0 * Unigram_English.evaluate(test_data1))
 
 Unigram_Brazilian.tag(mac_morpho.sents()[9])
 print 'Accuracy of Unigram tagger in Brazilian(Brazilian Portuguese): %4.1f%%' % (
-    100.0 * Unigram_Brazilian.evaluate(mac_morpho.tagged_sents()[1000:2000]))
+    100.0 * Unigram_Brazilian.evaluate(Braz_test_data1))
+    
+test_data2=brown.tagged_sents()[9000:10000]
+train_data2=brown.tagged_sents()[10000:19000]
 
-Bigram_tagger_English=nltk.BigramTagger(brown.tagged_sents()[:1000])
+Braz_test_data2=mac_morpho.tagged_sents()[9000:10000]
+Braz_train_data2=mac_morpho.tagged_sents()[10000:19000]
+
+Bigram_tagger_English=nltk.BigramTagger(train_data2)
 Bigram_tagger_English.tag(brown.sents()[9])
 print 'Accuracy of Bigram tagger in English: %4.1f%%' % (
-    100.0 * Bigram_tagger_English.evaluate(brown.tagged_sents()[1000:2000]))
+    100.0 * Bigram_tagger_English.evaluate(test_data2))
 
-Bigram_tagger_Brazilian=nltk.BigramTagger(mac_morpho.tagged_sents()[:1000])
+Bigram_tagger_Brazilian=nltk.BigramTagger(Braz_train_data2)
 Bigram_tagger_Brazilian.tag(mac_morpho.sents()[9])
 print 'Accuracy of Bigram tagger in Brazilian(Brazilian Portuguese): %4.1f%%' % (
-    100.0 * Unigram_Brazilian.evaluate(mac_morpho.tagged_sents()[1000:2000]))
+    100.0 * Unigram_Brazilian.evaluate(Braz_test_data2))
