01602a10505e69a307782deafaea8cc99316fbb8
Prokulevych K. <qqqkate@mail.ru>
2013-03-29 00:29:57 +0200

    задача 3 розділ 5

--- prokulevych-k/Prokulevych_Chapter5_№3.py
+++ prokulevych-k/Prokulevych_Chapter5_№3.py
@@ -1,3 +1,4 @@
+#Прокулевич К, ПРЛс-11, Задача №3, Розділ5
 # TODO
 # Comments ?
 # What  different  pronunciations  and  parts-of-speech  are involved?
@@ -10,3 +11,4 @@ text = nltk.word_tokenize("They wind back the clock, while we chase after the wi
 nltk.pos_tag(text)
 print nltk.pos_tag(text)
 # POS tagger опрацьовує послідовність слів і присвоює кожному слову тег частину мови. Результати програми показали, що слово 'They' - займенник, 'wind'-дієслово, 'back'-прислівник, 'the'-артикль, 'clock'-годинник, 'while'-сполучник, 'we'-займенник, 'chase'-дієслово, 'after'-сполучник, 'the'-артикль, 'wind'- іменник.   
+#тобто POS tagger визначає якою частиною мови є кожне слово
