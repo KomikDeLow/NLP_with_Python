469e482bea7703eafa452a4575c0b176eb217d85
Luchunka21 <luchunka21@gmail.com>
2013-03-28 17:41:54 +0200

    add coments

--- Shchudlo/Schudlo_6,2.py
+++ Shchudlo/Schudlo_6,2.py
@@ -3,18 +3,18 @@
 # It doesn't work
 # NameError: name 'test_set' is not defined
 
-
+# program defines gender of names
 import nltk
 import random
 from nltk.corpus import names
 
 Set_of_names = ([(name, 'male') for name in names.words('male.txt')]+
-            [(name, 'female') for name in names.words('female.txt')])
+            [(name, 'female') for name in names.words('female.txt')]) # building list of all names and they gender
 random.shuffle(Set_of_names)
 def gender_features(word):
-	return {'last_letter': word[-1]}
+	return {'last_letter': word[-1]} #extracting last letter of the name
 
-words_with_features = [(gender_features(n), g) for (n,g) in Set_of_names]
+words_with_features = [(gender_features(n), g) for (n,g) in Set_of_names] #building list of features for tegger
 test_data=words_with_features[:500]
 dev_test_data=words_with_features[500:1000]
 training_data=words_with_features[1000:]
@@ -23,7 +23,7 @@ classifier = nltk.NaiveBayesClassifier.train(training_data)
 print 'Accurasy of standart classifier: ',100.0*(nltk.classify.accuracy(classifier, dev_test_data)),'%'
 
 def gender_features2(word):
-	return {'two_last_letters': word[-2:]}
+	return {'two_last_letters': word[-2:]} # extracting two last letters
 featuresets2 = [(gender_features2(n), g) for (n,g) in Set_of_names]
 test_data2=featuresets2[:500]
 dev_test_data2=featuresets2[500:1000]
