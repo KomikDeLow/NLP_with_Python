8d8fbcbce06e2e74a2d35a8d61d2c12dad7a85dd
Luchunka21 <luchunka21@gmail.com>
2013-03-28 17:50:57 +0200

    devided data, added coment

--- Shchudlo/Shchudlo_10.py
+++ Shchudlo/Shchudlo_10.py
@@ -8,8 +8,8 @@ import nltk
 from nltk.corpus import brown
 brown_tagged_sents = brown.tagged_sents(categories='news')
 brown_sents = brown.sents(categories='news')
-unigram_tagger = nltk.UnigramTagger(brown_tagged_sents)
-print unigram_tagger.tag(['His', 'humorous', 'intention', 'to',
+unigram_tagger = nltk.UnigramTagger(brown_tagged_sents[:4000]) # training a unigran tagger
+print unigram_tagger.tag(['His', 'humorous', 'intention', 'to',  # taging my data, a sentence from internet
                     'describe', 'the', 'story', 'of', 'a', 'fictitious',
                     'trip', 'to', 'Mars', 'quickly', 'raised', 'eyebrows', 'as',
                     'his', 'Brooklyn,', 'New', 'York,', 'audience',
@@ -21,4 +21,6 @@ print unigram_tagger.tag(['His', 'humorous', 'intention', 'to',
                     'storytellers',',', 'as', 'well', 'as', 'through', 'the',
                     'lens', 'of', 'telescopes', 'that', 'returned', 'imprecise',
                     'images','.'])
-print 'Evaluation = ' ,unigram_tagger.evaluate(brown_tagged_sents)
+print 'Evaluation = ' ,unigram_tagger.evaluate(brown_tagged_sents[4000:]) # evaluating a tagger
+# Some words are not assigned a tag as our training data is not full, it contains only 4627 sentences wich may not contain words
+# from the sentence which we need to tag.
