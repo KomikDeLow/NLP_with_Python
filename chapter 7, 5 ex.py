6128eff4fc69f83148ff866a8e4886885954fb44
zakala <marija.zakala@gmail.com>
2013-03-30 23:21:28 +0200

    Create chapter 7, 5 ex.py

---
+++ chapter 7, 5 ex.py
@@ -0,0 +1,12 @@
+>>> import nltk
+>>> grammar = r"""
+  NP: {<DT|NN>?<VBG><NN>}"""
+>>> sentence = [("the", "DT"), ("receiving", "VBG"), ("end", "NN"),(",", ","),
+("assistant", "NN"), ("managing", "VBG"), ("editor", "NN")]
+>>> cp = nltk.RegexpParser(grammar)
+>>> print cp.parse(sentence)
+(S
+  (NP the/DT receiving/VBG end/NN)
+  ,/,
+  (NP assistant/NN managing/VBG editor/NN))
+>>>
