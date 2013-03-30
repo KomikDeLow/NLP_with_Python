01825eafb00469e7d3131b643ad2de3484d5295a
zakala <marija.zakala@gmail.com>
2013-03-30 19:24:52 +0200

    Create 6chapter. 4 ex.py
    На мою думку, ці слова є найбільш інформативними, тому що будь-яке з цих слів несе в собі конкретні значення і асоціації. Почувши одне з цих слів можна відразу сказати до яких слів воно належить (позитивних чи негативних). Найбільше, що мене здивувало серед цих результатів, це те, що поряд з прикметниками і прислівниками знаходяться імена акторів та назви видуманих героїв. Наприклад, Seagal (Стівен Сігал) зустрічається  у 8 раз частіше у негативному значенні ніж у позитивному, хоча, він зазвичай грає ролі “хороших хлопців”. Damon (Мет Деймон), теж актор і він зустрічається в 6 разів частіше в позитивному значені, ніж негативному. Слово jedi (Джедай) зустрічається в 4 раза частіше у позитивному значенні, ніж у негативному.  

---
+++ 6chapter. 4 ex.py
@@ -0,0 +1,17 @@
+>>> import nltk
+>>>  from nltk.corpus import movie_reviews
+>>> documents = [(list(movie_reviews.words(fileid)), category)
+       for category in movie_reviews.fileid.categories()
+	     for fileid in movie_reviews.fileids(category)]
+>>> random.shuffle(documents)
+>>> all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
+>>> word_features = all_words.keys() [:2000]
+>>> def document_features(document):
+	document_words = set(document)
+	features ={}
+	for word in word_features:
+		features['contains(%s)' % word] = (word in document_words)
+	return features
+>>> featuresets = [(document_features(d),c) for (d,c) in documents]
+>>> classifier = nltk.NaiveBayesClassifier.train(train_set)
+>>> Classifier.show_most_informative_features(30)
