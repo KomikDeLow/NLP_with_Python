cafc4276a1ea717f2276c0cd62f2d112035b8412
Mariia Komar <komar.mariia>
2013-03-30 14:09:43 +0200

    Chapter_7, Ex_8

---
+++ Komar_Mariia/Komar_Chapter_7,Ex_8_Chunker.py
@@ -0,0 +1,15 @@
+#Komar Mariia Als-11
+#Chapter_7, Ex_8
+#Develop a chunker for one of the chunk types in the CoNLL Chunking Corpus
+#using a regular expression–based chunk grammar RegexpChunk. Use any combination
+#of rules for chunking, chinking, merging, or splitting.
+
+import nltk
+grammar=""" # Create grammar  
+         NP:
+         {<DT>?<JJ>*<NN>*<NNS>*} #Chunk sequences of DT, JJ, NN, NNS
+         }<VBP|IN>+{             #Chink sequences of VBD and IN   
+         """
+cp= nltk.RegexpParser(grammar) #create a chunk parser test it on our example sentence
+sentence=[('Small', 'JJ'), ('beautiful', 'JJ'), ('cats', 'NNS'), ('stay', 'VBP'), ('on', 'IN'), ('the', 'DT'), ('yard', 'NN'), ('and', 'CC'), ('look', 'VBP'), ('on', 'IN'), ('the', 'DT'), ('blue', 'JJ'), ('sky', 'NN')]#test chunk parser on my sentence
+print cp.parse(sentence)#print result
