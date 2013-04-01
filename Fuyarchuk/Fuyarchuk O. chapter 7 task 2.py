82717b9e6af7217f349ac3975ab6dc0978ad7fef
olyafuyarchuk <ofuyarchuk@mail.ru>
2013-04-01 20:35:45 +0300

    chapter 7

---
+++ Fuyarchuk/Fuyarchuk O. chapter 7 task 2.py
@@ -0,0 +1,9 @@
+# Фуярчук О. Розділ 7 Задача 2
+import nltk
+sentence=[("much","JJ"),("sugar","NN"),("agile","JJ"),("boys","NNS"),
+          ("three","CD"),("flowers","NNS"),("beautiful","JJ"),("garden","NN")]
+grammar = "NP: {<DT>*<JJ>*<CD>*<NN.*>+}" # Define a chunk grammar
+bk=nltk.RegexpParser(grammar)# Using this grammar we create a chunk parser
+result=bk.parse(sentence)#Test this chunk on our example
+print result
+
