ec55a7548fea047a236cd3497507ad514178f7e3
Alexandr Antoniuk <antonyuk.aleksandr@mail.ru>
2013-04-01 21:25:56 +0300

    завдання 7, 5 юніт

---
+++ antoniuk/antoniuk7.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk import*
+d1={}.fromkeys(['help', 'room', 'red'])
+d2={}.fromkeys(['phone', 'ask', 'clean', 'member'])
+d1['help']='V'
+d1['room']='N'
+d1['red']='A'
+d2['phone']='N'
+d2['ask']='V'
+d2['clean']='A'
+d2['member']='N'
+d1.update(d2)
+d1
+print d1
