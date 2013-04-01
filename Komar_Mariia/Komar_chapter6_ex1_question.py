245e3748241801ab0de78d8067239ef625429556
Mariia Komar <komar.mariia>
2013-04-01 12:57:34 +0300

    Chapter_6, Ex_1

---
+++ Komar_Mariia/Komar_chapter6_ex1_question.py
@@ -0,0 +1,23 @@
+#Komar Mariia Als-11
+#Chapter_6, Ex_1
+
+#Read up on one of the language technologies mentioned in this section, such as
+#word sense disambiguation, semantic role labeling, question answering, machine
+#translation, or named entity recognition. Find out what type and quantity of annotated
+#data is required for developing such systems. Why do you think a large
+#amount of data is required?
+
+question1=('Question:')
+print question1
+question=('Read up on one of the language technologies mentioned in this section, such as word sense disambiguation, semantic role labeling, question answering, machine translation, or named entity recognition. Find out what type and quantity of annotated data is required for developing such systems. Why do you think a large amount of data is required?')
+print question
+answer1=('Answer:')
+print answer1
+answer=('It requires a large amount of data in order to correctly identify what part of speech is the word in text,and using the context, find different meanings of the word.')
+print answer
+
+#It requires a large amount of data in order to correctly identify
+#what part of speech is the word in text,
+#and using the context, find different meanings of the word.
+
+
