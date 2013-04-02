68e9665487ef1e25305049d37c402d28b008d351
Veronika <Nika_gold@i.ua>
2013-04-02 20:41:02 +0400

    Chapter_5_15b

---
+++ Klantsak_Chapter_5_15b.py
@@ -0,0 +1,17 @@
+# yakuj vidsotok tupiv sliv markyetsya tumu samumu tegamu
+import nltk
+from nltk.corpus import brown
+def persentage(text):
+    data=nltk.ConditionalFreqDist((word.lower(),tag) for (word,tag)in text)
+    t=0
+    a=0
+    for word in data.conditions():
+        if len(data[word])==1:
+            a+=1
+            t+=1
+            return a*100/t
+
+
+
+print persentage(nltk.corpus.brown.tagged_words())
+print persentage(nltk.corpus.treebank.tagged_words())
