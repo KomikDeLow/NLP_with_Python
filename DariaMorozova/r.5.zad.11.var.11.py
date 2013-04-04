b2599f6385723a2ba4c6c667aa5651de3af2d913
Дарія Морозова <dashysik.nirvana@gmail.com>
2013-04-04 23:24:05 +0300

    5 розділ 11 задача

---
+++ DariaMorozova/r.5.zad.11.var.11.py
@@ -0,0 +1,29 @@
+# Rozdil 5 zad. 11
+# Morozova Daria PRLs-12
+# Variant 11
+
+import nltk
+from nltk.corpus import *
+help(nltk.AffixTagger)
+train_sents = [
+[('Before','ADV'), ('giving','VG'), ('a','DET'), ('test','N'), ('the','DET')],
+[('teacher','N'), ('should','MOD'), ('make','V'), ('sure','ADJ')],
+[('that','CNJ'), ('the','DET'), ('students','N'), ('are','V'), ('prepared','VD')],
+]
+text = 'Otherwise they can fail the test.'
+affix_tagger = nltk.AffixTagger(train_sents, affix_length=-2, min_stem_length=0)
+tags = affix_tagger.tag(nltk.word_tokenize(text))
+print tags
+affix_tagger = nltk.AffixTagger(train_sents, affix_length=-1, min_stem_length=2)
+tags = affix_tagger.tag(nltk.word_tokenize(text))
+print tags
+affix_tagger = nltk.AffixTagger(train_sents, affix_length=2, min_stem_length=2)
+tags = affix_tagger.tag(nltk.word_tokenize(text))
+print tags
+affix_tagger = nltk.AffixTagger(train_sents, affix_length=2, min_stem_length=5)
+print tags
+
+
+
+
+                                
