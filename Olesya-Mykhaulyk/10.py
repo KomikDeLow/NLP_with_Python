9221020fc68d694a25e26d03281d9254b2b63c22
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-04-06 00:04:22 +0300

    5.10

--- Olesya-Mykhaulyk/10.py
+++ Olesya-Mykhaulyk/10.py
@@ -4,9 +4,9 @@
 import nltk
 from nltk.corpus import brown # importyemo korpus brown
 brown_sents=brown.sents(categories='humor')# vuburaem kategoriy humor
-brown_tagged_sents=brown.tagged_sents(categories='humor')#rozbuvaemo na tegu
-unigram_tagger=nltk.UnigramTagger(brown_tagged_sents)#zadaemo parametr
+brown_tagged_sents=brown.tagged_sents(categories='humor')#rozbuvaemo tekst danoi kategorii na tegu
+unigram_tagger=nltk.UnigramTagger(brown_tagged_sents)#zadaemo parametr dla trenyvanja
 unigram_tagger.tag(brown_sents[50])
-brown_tagged_sents_1=brown.tagged_sents(categories='news')#nakladaem na novui tekst z rozdily news
+brown_tagged_sents_1=brown.tagged_sents(categories='news')#zapyskaem analizator na teksti kategorii news
 brown_sents_1=brown.sents(categories='news')
 unigram_tagger.tag(brown_sents_1[50])# zna4enna NONE prusvoyetsa vsim slovam, jaki ne zystri4aytsa v tekstah na osnovi jakuh provoduvsa analiz(tobto tsi slova vidsytni y spusky nai4astotniwuh)
