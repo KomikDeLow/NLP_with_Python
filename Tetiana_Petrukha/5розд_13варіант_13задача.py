772060a7948bb86df2b2898848db515636f4d044
Tetiana-Petrukha <taniapetrukha@gmail.com>
2013-04-06 00:52:53 +0300

    Chapter 5_ex 13

--- Tetiana_Petrukha/5розд_13варіант_13задача.py
+++ Tetiana_Petrukha/5розд_13варіант_13задача.py
@@ -1,9 +1,16 @@
+# Petrukha Tetiana Als-11
+# Chapter_7, Ex_1
+
+# We can use a dictionary to specify the values to be substituted into a formatting string.
+# Read Python’s library documentation for formatting strings (http://docs.python.org/lib/typesseq-strings.html)
+# and use this method to display today’s date in two different formats.
+
 # TODO
 # comments ??
 #
 
 import datetime
-now=datetime.date.today().strftime("%B %d, %Y")
-print now
-today=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
-print today
+now=datetime.date.today().strftime("%B %d, %Y") # get the date from the computer
+print now # print date
+today=datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y") # All the letter after a "%" represent a format for something :
+print today # print time and date
