59cec438a649948e06bdd70b594ce4aa555b0c03
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-04-06 00:00:28 +0300

    5.24

--- Olesya-Mykhaulyk/24.py
+++ Olesya-Mykhaulyk/24.py
@@ -8,7 +8,7 @@ brown_sents=brown.sents()
 brown_tagged_sents=brown.tagged_sents() #dostypaemos do re4en
 size=int(len(brown_tagged_sents)*0,9)
 train_sents=brown_tagged_sents[:size] # dani na jakuh provodutsa trenyvanja
-    for n in range(6):
+    for n in range(6):  #zbilwyemo n do 6 i robumo otsinky robotu ngramma
         ngram=nltk.NgramTagger(n,train=brown_tagged_sents)
         print n, ngram.evaluate(brown_tagged_sents[size:]) # otsinka robotu i vuvedenja rezyltativ
 
