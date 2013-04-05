899ca23fe9f20c75bf889fe62bc4c7916de1f0df
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-04-06 01:21:48 +0300

    Unit 7

---
+++ Mykhaulyk_Olesya/7.2.py
@@ -0,0 +1,11 @@
+#Olesya Mykhaulyk ALs-12
+#7.2
+#          jj         nns
+#              cd     nns
+#  dt      jj         nns
+#<dt>?<jj>?<cd>?<nns>
+# abo mogluvui inwui variant:
+#          jj    nns
+#          cd    nns
+#   dt     jj    nns
+#<dt>?<jj|cd><nns>
---
+++ Mykhaulyk_Olesya/7.6.py
@@ -0,0 +1,20 @@
+#Olesya Mykhaulyk ALs-12
+# 7.6
+# I have found 3 ways to deal with this task
+# the first one is:
+#                      nnp     cc     nnp
+#   dt       prp$      nns     cc     nns
+#            nn        nns     cc     nns
+#<dt>?<prp$|nn>?<nn.><cc><nn.>
+#the second one is:
+# nnp                          cc     nnp
+# dt      prp$       nns       cc     nns
+# nn                 nns       cc     nns
+# <nn.|dt><prp$>?<nns>?<cc><nn.>
+# the third one is:
+#  nnp                              cc     nnp
+#          dt     prp$     nns      cc     nns
+#  nn                      nns      cc     nns
+#<nn.>?<dt>?<prp$>?<nns>?<cc><nn.>
+# also there may be more possible ways to solve the problem.
+#it depens on combinations we create
