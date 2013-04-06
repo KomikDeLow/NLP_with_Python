78d15964eb86c12fe7e5699834a5431c30f2181b
Tetiana-Petrukha <taniapetrukha@gmail.com>
2013-04-06 16:17:16 +0300

    Chapter 5 ex(21, 28)

--- Tetiana_Petrukha/5розд_13варіант_21задача.py
+++ Tetiana_Petrukha/5розд_13варіант_21задача.py
@@ -1,19 +1,24 @@
 # TODO
 # Comments?
 # Two different for loops for creating fd? Why?
+
+# Petrukha Tetiana Als-11
+# Chapter_5, Ex_21
+
+# In Table 3-1, we saw a table involving frequency counts for the verbs adore,
+# love, like, and prefer, and preceding qualifiers such as really.
+# Investigate the full range of qualifiers (Brown tag QL) that appear before these four verbs.
+
 import nltk
-fd = nltk.FreqDist()
+fd = nltk.FreqDist() # frequency distribution
 text=nltk.corpus.brown.words(categories='romance')
 dic=['']
 for (wd, tg) in nltk.corpus.brown.tagged_words():
-	if tg[:2] == 'QL':
-		fd.inc(wd + "=" + tg)
-for (wd, tg) in nltk.corpus.brown.tagged_words():
-	if tg[:2] == 'QL':
+	if tg[:2] == 'QL': # garner statistics about the tags
 		fd.inc(wd)
-verbs=['adore', 'love', 'like', 'prefer']
+verbs=['adore', 'love', 'like', 'prefer'] # list verbs
 for i in range(len(text)):
 	if text[i]in fd.keys()and text[i+1]in verbs and (text[i]+' '+text[i+1]) not in dic:
-		dic.append(text[i]+' '+text[i+1])
+		dic.append(text[i]+' '+text[i+1]) # create a list of our verbs
 
 print dic
--- Tetiana_Petrukha/5розд_13варіант_28задача.py
+++ Tetiana_Petrukha/5розд_13варіант_28задача.py
@@ -1,13 +1,29 @@
 # TODO
 # Comments?
 # I didn't see answer
+
+# Petrukha Tetiana Als-11
+# Chapter_5, Ex_28
+
+# Experiment with taggers using the simplified tagset.
+# Such a tagger has fewer distinctions to make, but much less information on
+# which to base its work. Discuss your findings.
+
 import nltk
-st = nltk.corpus.treebank.tagged_words(simplify_tags=True)
-word_tag_fd = nltk.FreqDist(st)
-noun=[word + "/" + tag for (word, tag) in word_tag_fd if tag.startswith('N')]
-print noun[:20]
+st = nltk.corpus.treebank.tagged_words(simplify_tags=True) # here we are using the simplified tagset
+word_tag_fd = nltk.FreqDist(st) # frequency distribution
+noun=[word + "/" + tag for (word, tag) in word_tag_fd if tag.startswith('N')] # sort all the noun by frequency
+print noun[:20] # print noun
 print('')
-nst = nltk.corpus.treebank.tagged_words(simplify_tags=False)
+nst = nltk.corpus.treebank.tagged_words(simplify_tags=False) # here we aren't using the simplified tagset
 word_tag_fd = nltk.FreqDist(nst)
 us_noun=[word + "/" + tag for (word, tag) in word_tag_fd if tag.startswith('N')]
 print us_noun [:20]
+
+# Not all corpora employ the same set of tags. If we want to avoid the complications
+# of these tagsets, so we use a built-in mapping to a simplified tagset
+# But they are much less informative.
+# For example we take a nouns tags. At bouth tagset(Simplified and Unsimplified)
+# we can see plural nouns-NS or proper nouns-NP
+# But Simplified tagset don't have suffix modifiers: -NC for citations,
+# -HL for words in headlines, and -TL for titles 
