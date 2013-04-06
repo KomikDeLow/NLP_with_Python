cbb0b600c83b363cc11ce6889bdabd2548bcaf96
Tetiana-Petrukha <taniapetrukha@gmail.com>
2013-04-06 17:39:41 +0300

    Chapter 6 ex (4, 7)

--- Tetiana_Petrukha/Petrukha_6_4(most informative features).py
+++ Tetiana_Petrukha/Petrukha_6_4(most informative features).py
@@ -1,24 +1,33 @@
 # TODO
 # Comments?
-# 
+
+# Petrukha Tetiana Als-11
+# Chapter_6, Ex_4
+
+# Using the movie review document classifier discussed in this chapter, generate
+# a list of the 30 features that the classifier finds to be most informative.
+# Can you explain why these particular features are informative?
+# Do you find any of them surprising?
+
 import nltk
-from nltk.corpus import movie_reviews
+from nltk.corpus import movie_reviews # import movie_reviews corpus
 documents=[(list(movie_reviews.words(fileid)),category)
 	   for category in movie_reviews.categories()
 	   for fileid in movie_reviews.fileids(category)]
+# construct a list of documents, labeled with the appropriate categories.
 import random
-random.shuffle(documents)
+random.shuffle(documents) # shuffle positive and negative features
 all_words=nltk.FreqDist(w.lower() for w in movie_reviews.words())
 word_features=all_words.keys()[:2000]
-def document_features(document):
-	document_words=set(document)
-	features={}
+def document_features(document): # take document
+	document_words=set(document) # take the word of this document
+	features={} # look for features
 	for word in word_features:
-		features['contains(%s)' %word]=(word in document_words)
+		features['contains(%s)' %word]=(word in document_words) # check is the word contained in the document or not
 	return features
     
 featuresets=[(document_features(d),c) for (d,c) in documents]
-train_set, test_set=featuresets[100:],featuresets[:100]
-classifier=nltk.NaiveBayesClassifier.train(train_set)
-print nltk.classify.accuracy(classifier,test_set)
-print classifier.show_most_informative_features(30)
+train_set, test_set=featuresets[100:],featuresets[:100] # divide data for train and test
+classifier=nltk.NaiveBayesClassifier.train(train_set) # train classifier
+print nltk.classify.accuracy(classifier,test_set) # assess the accuracy of his work
+print classifier.show_most_informative_features(30) # print a list of the 30 features that the classifier finds to be most informative
--- Tetiana_Petrukha/Petrukha_6_7(labeling dialogue acts).py
+++ Tetiana_Petrukha/Petrukha_6_7(labeling dialogue acts).py
@@ -1,26 +1,38 @@
 # TODO
 # Comments?
-# 
+
+# Petrukha Tetiana Als-11
+# Chapter_6, ex_7
+
+# The dialogue act classifier assigns labels to individual posts, without considering
+# the context in which the post is found. However, dialogue acts are highly dependent
+# on context, and some sequences of dialogue act are much more likely than others.
+# For example, a ynQuestion dialogue act is much more likely to be answered by a yanswer
+# than by a greeting. Make use of this fact to build a consecutive classifier
+# for labeling dialogue acts. Be sure to consider what features might be useful.
+# See the code for the consecutive classifier for part-of-speech tags in Example 6-5
+# to get some ideas.
+
 
 import nltk
-posts = nltk.corpus.nps_chat.xml_posts()
+posts = nltk.corpus.nps_chat.xml_posts() # importing posts
 history=[]
 def pos_features(post,i,history):
-	features = {}
+	features = {}  # creating function of obtaining features
 	for word in nltk.word_tokenize(post):
-		features['contains(%s)' % word.lower()] = True
+		features['contains(%s)' % word.lower()] = True # check what words the post contains
 		if i == 0:
 			features["prev-class"] = "<START>"
 		else:
-			features["prev-class"] = history[i-1]
+			features["prev-class"] = history[i-1] # obtaining class of the previous post from list "history"
 		return features
 	    
-featuresets=[]
-for i, post in enumerate(posts):
+featuresets=[] # creating an empty list for the features
+for i, post in enumerate(posts): # a cycle running throught the posts and appendind the features
         featuresets.append((pos_features(post.text, i, history), post.get('class')))
-        history.append(post.get('class'))
+        history.append(post.get('class')) # append list of all types of posts
 
-size=int(len(featuresets)*0.1)
-train_set, test_set = featuresets[size:], featuresets[:size]
-classifier = nltk.NaiveBayesClassifier.train(train_set)
-print nltk.classify.accuracy(classifier, test_set)
+size=int(len(featuresets)*0.1) # choose data size for testing
+train_set, test_set = featuresets[size:], featuresets[:size] # divide data for train and test
+classifier = nltk.NaiveBayesClassifier.train(train_set) # create a classifier
+print nltk.classify.accuracy(classifier, test_set) # evaluate the accuracy of the work
