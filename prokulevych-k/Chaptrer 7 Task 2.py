497e61da4d3877b3b903d6c24774622b01b7155c
Prokulevych K. <qqqkate@mail.ru>
2013-04-07 14:53:40 +0300

    розділ 7 задача 2

--- prokulevych-k/Chaptrer 7 Task 2.py
+++ prokulevych-k/Chaptrer 7 Task 2.py
@@ -4,8 +4,8 @@
 #Прокулевич К. ПРЛс-11, Розділ 7, Задача 2
 #Regular expression–based NP chunker
 import nltk
-sentence = [("many", "JJ"), ("researchers", "NNS"), ("two", "CD"), ("weeks", "NNS"), ("both", "DT"), ("new", "JJ"), ("positions", "NNS")]
-grammar = "NP: {<DT>*<JJ>*<CD>*<PRP>*<NN.*>+}" # creating chunk grammar 
+sentence=[("Jim", "NNP"), ("saw", "VBD"), ("beautiful", "JJ"), ("red", "JJ"), ("flowers", "NNS"), ("in", "IN"), ("the", "DT"), ("garden","NN")]
+grammar = "NP: {<DT>*<JJ>*<NN.*>+}" # creating chunk grammar 
 cp = nltk.RegexpParser(grammar)#create a chunk parser using a grammar
 result = cp.parse(sentence)#test chunk parser on sentence
 print result
