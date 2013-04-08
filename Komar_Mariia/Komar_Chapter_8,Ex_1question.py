f113406f58083489a4706d40641bc3807366e348
Mariia Komar <komar.mariia>
2013-04-08 20:17:37 +0300

    Chapter_8,Ex_1,4

---
+++ Komar_Mariia/Komar_Chapter_8,Ex_1question.py
@@ -0,0 +1,19 @@
+#Komar Mariia Als-11
+#Chapter_8, Ex_1
+
+#Can you come up with grammatical sentences that probably have never
+#been uttered before? (Take turns with a partner.) What does this tell
+#you about human language?
+
+question1=('Question:')
+print question1
+question=('Can you come up with grammatical sentences that probably have never been uttered before? (Take turns with a partner.) What does this tell you about human language?')
+print question
+answer1=('Answer:')
+print answer1
+answer=('Ми можемо придумати речення які ніколи ніхто не озвучував, це свідчить про те що людський розум немає обмежень в творенні слів, навідміну від компютера')
+print answer
+
+#Ми можемо придумати речення які ніколи ніхто не озвучував,
+#це свідчить про те що людський розум немає обмежень в творенні слів,
+#навідміну від компютера
