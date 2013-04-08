80c49f63e6c33cff894192566414c8c5c18fe510
Mariia Komar <komar.mariia>
2013-04-08 09:07:53 +0300

    Chapter_7_Ex_2,6,8

--- Komar_Mariia/Komar_Chapter_7,Ex_8_Chunker.py
+++ Komar_Mariia/Komar_Chapter_7,Ex_8_Chunker.py
@@ -16,3 +16,9 @@ grammar=""" # Create grammar
 cp= nltk.RegexpParser(grammar) #create a chunk parser test it on our example sentence
 sentence=[('Small', 'JJ'), ('beautiful', 'JJ'), ('cats', 'NNS'), ('stay', 'VBP'), ('on', 'IN'), ('the', 'DT'), ('yard', 'NN'), ('and', 'CC'), ('look', 'VBP'), ('on', 'IN'), ('the', 'DT'), ('blue', 'JJ'), ('sky', 'NN')]#test chunk parser on my sentence
 print cp.parse(sentence)#print result
+
+from nltk.corpus import conll2000 #import conll2000 corpus for making evaluation
+cp = nltk.RegexpParser(grammar) #create a chunk parser test it on our example sentence
+test_sents = nltk.corpus.conll2000.chunked_sents('test.txt', chunk_types=('NP',))
+print  'Accuracy=', nltk.chunk.accuracy(cp, test_sents) #print evaluation
+
--- Komar_Mariia/Komar_tag patterns_ch7_ ex6.py
+++ Komar_Mariia/Komar_tag patterns_ch7_ ex6.py
@@ -6,7 +6,9 @@
 #Write one or more tag patterns to handle coordinated noun phrases
 
 import nltk
-sentence = [ ("July", "NNP"), ("and", "CC"), ("August", "NNP"),("all", "DT"), ("your", "PRP$"), ("managers", "NNS"),("and","CC"),("supervisors","NNS"),("company","NN"),("courts","NNS"),("and","CC"),("adjudicators","NNS")]#create sentence
+sentence = [ ("All", "DT"), ("your", "PRP$"), ("managers", "NNS"),("and","CC"),
+             ("supervisors","NNS"), ("arrived", "VBD"), ("at","IN"),
+             ("July", "NNP"), ("and", "CC"), ("August", "NNP")]#create sentence
 grammar = "NP:{<DT>?<PRP$>?<NN.*>+<CC><NN.*>*}" #create ragular expression
 cp = nltk.RegexpParser(grammar) #create a chunk parser test it on our example sentence
 result = cp.parse(sentence)#test chunk parser on my sentence
