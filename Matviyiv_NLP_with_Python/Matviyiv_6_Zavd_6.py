1ec9378407ade6b9cf9f8508a716a7f3676bfd44
Michael Matviyiv <mriynuk99@gmail.com>
2013-04-09 18:48:31 +0300

    Added comments and deleted unnecesary lines
    in task 30 deleted lines with unnessesary tagging and made the program
    simpler, devided train and test data

--- Matviyiv_NLP_with_Python/Matviyiv_6_Zavd_6.py
+++ Matviyiv_NLP_with_Python/Matviyiv_6_Zavd_6.py
@@ -1,20 +1,22 @@
 # TODO
 # Comments?
-# I see this program in Shudlo branch today !!!
+# I see this program in Shudlo branch today !!! (This task was done together with her, it just was submited in different time. I can redo this task if nessasery.)
+
+# Building a classifier which should predict wich of these:(storong,powerfull) should be used
 import nltk
 import random
 from nltk.corpus import brown
-tagged_words=brown.tagged_words()
-bigram_list=nltk.bigrams(tagged_words)
-bigram_needed=[w for w in bigram_list if w[0][0].lower() in ['strong','powerful']]
-def feature11((w,p)):
-	return {'word':p[0],'Pos':p[1]}
+tagged_words=brown.tagged_words() # building a list of all words
+bigram_list=nltk.bigrams(tagged_words)# bulding a bigram list of all words
+bigram_needed=[w for w in bigram_list if w[0][0].lower() in ['strong','powerful']] # taking out bigrams that have "strong" and "powerfull" in them
+def feature11((w,p)): # building a function which gets feature of a word (words + position)
+	return {'word':p[0],'Pos':p[1]} # it returns a dictionary
 featureSet=[]
 for word in bigram_needed:
-	featureSet.append((feature11(word),word[0][0]))
+	featureSet.append((feature11(word),word[0][0])) # building a cycle to create a set of data with features for the classifier
 test_data=featureSet[:120]
-train_data=featureSet[120:]
+train_data=featureSet[120:]# dividing test and train data
 
-classifier = nltk.NaiveBayesClassifier.train(train_data)
+classifier = nltk.NaiveBayesClassifier.train(train_data) # training the classifier
 
-print 'Clasifier accuraci: ',100.0 * nltk.classify.accuracy(classifier, test_data),'%'
+print 'Clasifier accuraci: ',100.0 * nltk.classify.accuracy(classifier, test_data),'%' # evaluating the builded classifier
--- Matviyiv_NLP_with_Python/Matviyvi_5_zavd30.py
+++ Matviyiv_NLP_with_Python/Matviyvi_5_zavd30.py
@@ -1,22 +1,26 @@
 # TODO
 # Comments?
 # test and train data?
+
+# Changing low-frequency words with UNK, but leaving the tags as they are, then evaluating the classifiers
+# to see if this gave something to the accuracy.
 import nltk
 from nltk.corpus import brown
 words_tagged=brown.tagged_words(categories='news')
-fd=nltk.FreqDist(words_tagged)
-word_list=fd.keys()
-word_list.reverse()
-words_to_change=word_list[:3000]
+fd=nltk.FreqDist(words_tagged) # building a frequency list of the used words
+word_list=fd.keys() # building a set of words 
+word_list.reverse() # reversing this set
+words_to_change=word_list[:3000] # taking out 3000 low-frequency words
 words_to_change_list=[]
 for w in words_to_change:
-	words_to_change_list.append(w[0])
+	words_to_change_list.append(w[0]) # building a cycle to take out words without their tags
 
-sents_train=brown.tagged_sents(categories='news')
+sents_train_for_cycle=brown.tagged_sents(categories='news')[:4000]
+
 sent_with_changes=[]
 sent_one=[]
 counter=0
-for sent in sents_train:
+for sent in sents_train_for_cycle: # building a cycle which returns sentenses with changed low-frequency words to UNK
 	for word in sent:
 		if word[0] in words_to_change_list:
 			sent_one.append(('UNK',word[1]))
@@ -26,41 +30,30 @@ for sent in sents_train:
 	sent_with_changes.append(sent_one)
 	sent_one=[]
 
+sents_train=sents_train_for_cycle[:3000] # dividing data to train and test wich has no changed words
+sents_test=sents_train_for_cycle[3000:]
+
+sent_with_changes_train=sent_with_changes[:3000] # dividing data with changed words
+sent_with_changes_test=sent_with_changes[3000:]
+
 print counter,' words were changed to UNK in training sentences.'
-words_to_tag=brown.words()[:3000]
-Bigram_tagger=nltk.BigramTagger(sents_train)
-Bigram_tagger_with_changes=nltk.BigramTagger(sent_with_changes)
+
+Bigram_tagger=nltk.BigramTagger(sents_train) # Training taggers
+Bigram_tagger_with_changes=nltk.BigramTagger(sent_with_changes_train)
+
+print 'Accuracy Bigram_tagger (with normal unchanged words): %4.1f%%' % (
+    100.0 * Bigram_tagger.evaluate(sents_test)) # Evaluating taggers
+print 'Accuracy Bigram_tagger_with_changes (with changed low-frequency words): %4.1f%%' % (
+    100.0 * Bigram_tagger_with_changes.evaluate(sent_with_changes_test))
 
 
-Bigram_tagger.tag(brown.sents()[66])
-Bigram_tagger_with_changes.tag(brown.sents()[66])
+Unigram_tagger=nltk.UnigramTagger(sents_train) # Training taggers
+Unigram_tagger_with_changes=nltk.UnigramTagger(sent_with_changes_train)
 
-print 'Accuracy Bigram_tagger (on brown sents): %4.1f%%' % (
-    100.0 * Bigram_tagger.evaluate(brown.tagged_sents()[:6000]))
-print 'Accuracy Bigram_tagger_with_changes (on brown sents): %4.1f%%' % (
-    100.0 * Bigram_tagger_with_changes.evaluate(brown.tagged_sents()[:6000]))
 
-Bigram_tagger.tag(words_to_tag)
-Bigram_tagger_with_changes.tag(words_to_tag)
-print 'Accuracy Bigram_tagger (on 3000 words): %4.1f%%' % (
-    100.0 * Bigram_tagger.evaluate(brown.tagged_sents()[:6000]))
-print 'Accuracy Bigram_tagger_with_changes (on 3000 words): %4.1f%%' % (
-    100.0 * Bigram_tagger_with_changes.evaluate(brown.tagged_sents()[:6000]))
+print 'Accuracy Unigram_tagger (with normal unchanged words): %4.1f%%' % (
+    100.0 * Unigram_tagger.evaluate(sents_test)) # Evaluating taggers
+print 'Accuracy Unigram_tagger_with_changes (with changed low-frequency words): %4.1f%%' % (
+    100.0 * Unigram_tagger_with_changes.evaluate(sent_with_changes_test))
 
-Unigram_tagger=nltk.UnigramTagger(sents_train)
-Unigram_tagger_with_changes=nltk.UnigramTagger(sent_with_changes)
-Unigram_tagger.tag(brown.sents()[66])
-Unigram_tagger_with_changes.tag(brown.sents()[66])
 
-print 'Accuracy Unigram_tagger (on brown sents): %4.1f%%' % (
-    100.0 * Unigram_tagger.evaluate(brown.tagged_sents()[:6000]))
-print 'Accuracy Unigram_tagger_with_changes (on brown sents): %4.1f%%' % (
-    100.0 * Unigram_tagger_with_changes.evaluate(brown.tagged_sents()[:6000]))
-
-Unigram_tagger.tag(words_to_tag)
-Unigram_tagger_with_changes.tag(words_to_tag)
-
-print 'Accuracy Unigram_tagger (on 3000 words): %4.1f%%' % (
-    100.0 * Unigram_tagger.evaluate(brown.tagged_sents()[:6000]))
-print 'Accuracy Unigram_tagger_with_changes (on 3000 words): %4.1f%%' % (
-    100.0 * Unigram_tagger_with_changes.evaluate(brown.tagged_sents()[:6000]))
