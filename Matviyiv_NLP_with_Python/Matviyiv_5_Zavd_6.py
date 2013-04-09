af1fabd104a7f001f9856995e1d9ca2260a40d7d
Michael Matviyiv <mriynuk99@gmail.com>
2013-04-09 17:41:17 +0300

    Comments added

--- Matviyiv_NLP_with_Python/Matviyiv_5_Zavd_6.py
+++ Matviyiv_NLP_with_Python/Matviyiv_5_Zavd_6.py
@@ -1,14 +1,14 @@
 # TODO
 # Comments?
 #
-
-dict1={}
+# Deleting an element of a dictionary using function "del"
+dict1={} # creating an empty dictionary
 dict1['colorless'] = 'ADJ'
 dict1['ideas'] = 'N'
 dict1['sleep'] = 'V'
-dict1['furiously'] = 'ADV'
+dict1['furiously'] = 'ADV'# adding elements to a dictionary
 print 'Dictionary========================'
 print dict1
-del dict1['sleep']
+del dict1['sleep'] # deleting an entry using "del" function
 print 'Deleted entry (sleep)'
-print dict1
+print dict1 # printing out the dictionary
