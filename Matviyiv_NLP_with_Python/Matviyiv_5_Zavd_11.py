bbbee6fd7084487e5af9d82fdd5eacc2efaecbbb
Michael Matviyiv <mriynuk99@gmail.com>
2013-04-09 17:53:05 +0300

    TODO executed

--- Matviyiv_NLP_with_Python/Matviyiv_5_Zavd_11.py
+++ Matviyiv_NLP_with_Python/Matviyiv_5_Zavd_11.py
@@ -1,23 +1,29 @@
 #TODO
 #You must divide data on train and test
 #
+#  Training an Affix Tagger and running it on some new text.
 import nltk
 from nltk.corpus import brown
 brown_tagged_sents = brown.tagged_sents(categories='news')
-aff_tagger=nltk.AffixTagger(brown_tagged_sents)
+train_data=brown_tagged_sents[:4000]
+test_data=brown_tagged_sents[4000:]
+
+aff_tagger=nltk.AffixTagger(train_data)
 print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
                       'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
                       'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
                       'that', 'students', 'of', 'pre-intermediate', 'level', 'can', 'easily',
                       'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
+
+
 print '==============================================================='
 print 'Affix tagger size = ',aff_tagger.size()
-print 'Sentences used to evaluete = ',len(brown.tagged_sents()[:1000])
-print 'Evaluation = ',aff_tagger.evaluate(brown.tagged_sents()[:1000])
+print 'Sentences used to evaluete = ',len(test_data)
+print 'Evaluation = ',aff_tagger.evaluate(test_data)
 print '==============================================================='
 
-brown_tagged_sents = brown.tagged_sents(categories='news')
-aff_tagger=nltk.AffixTagger(brown_tagged_sents,affix_length=-2)
+
+aff_tagger=nltk.AffixTagger(train_data,affix_length=-2)
 print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
                       'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
                       'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
@@ -25,12 +31,12 @@ print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your'
                       'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
 print '==============================================================='
 print 'Affix length changed to = -2 ','Affix tagger size = ',aff_tagger.size()
-print 'Sentences used to evaluete = ',len(brown.tagged_sents()[:1000])
-print 'Evaluation = ',aff_tagger.evaluate(brown.tagged_sents()[:1000])
+print 'Sentences used to evaluete = ',len(test_data)
+print 'Evaluation = ',aff_tagger.evaluate(test_data)
 print '==============================================================='
 
-brown_tagged_sents = brown.tagged_sents(categories='news')
-aff_tagger=nltk.AffixTagger(brown_tagged_sents,min_stem_length=4)
+
+aff_tagger=nltk.AffixTagger(train_data,min_stem_length=4)
 print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your',
                       'reading', 'skills.', 'It', 'contains', 'simplified', 'English',
                       'texts', 'which', 'are', 'written', 'in', 'easy', 'grammar,', 'so',
@@ -38,6 +44,6 @@ print aff_tagger.tag(['In', 'this', 'section,', 'you', 'can', 'practise', 'your'
                       'understand.', 'Have', 'fun', 'and', 'enjoy', 'your', 'reading'])
 print '==============================================================='
 print 'Minimum stem length changed to = 4 ','Affix tagger size = ',aff_tagger.size()
-print 'Sentences used to evaluete = ',len(brown.tagged_sents()[:1000])
-print 'Evaluation = ',aff_tagger.evaluate(brown.tagged_sents()[:1000])
+print 'Sentences used to evaluete = ',len(test_data)
+print 'Evaluation = ',aff_tagger.evaluate(test_data)
 print '==============================================================='
