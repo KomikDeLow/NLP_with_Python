8c41c2d4bb4a26e20f8292840d6bac1df8975c60
Дарія Морозова <dashysik.nirvana@gmail.com>
2013-04-18 22:02:37 +0300

    розділ 6. задача 2

---
+++ DariaMorozova/zad.2_rozdil 6.py
@@ -0,0 +1,28 @@
+#
+# zad. 2 rozdil 6
+# Daria Morozova
+# var.11
+
+
+import nltk
+from nltk.corpus import names
+import random
+def gender_features(name):       # morphological characteristics of feature set
+        features={}
+	features['firstletter']=name[0].lower() #first letter of the name
+	features['lastletter']=name[-1:].lower() # last letter of the name
+	for letter in 'abcdefghijklmnopqrstuvwxyz':
+		features["count(%s)" % letter] = name.lower().count(letter)
+		features["has(%s)" % letter] = (letter in name.lower())
+		return features
+names = ([(name, 'male') for name in names.words('male.txt')] +
+       [(name, 'female') for name in names.words('female.txt')]) #building a set of data
+
+random.shuffle(names) # shuffling the set of names randomly
+featuresets = [(gender_features(n), g) for (n,g) in names] #building data for the classifier
+test_set=featuresets[:500]    #dividing data on test_set and train_set
+dev_test_set=featuresets[500:1000]
+train_set=featuresets[1000:]
+classifier = nltk.NaiveBayesClassifier.train(train_set) 
+print nltk.classify.accuracy(classifier, dev_test_set) #evaluating accuracy
+print nltk.classify.accuracy(classifier, test_set)
