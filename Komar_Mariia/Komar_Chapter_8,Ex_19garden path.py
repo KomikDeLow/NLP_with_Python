04fef03a4a664705f9b2966e66a80fef98b30726
Mariia Komar <komar.mariia>
2013-04-21 20:53:19 +0300

    Chapter_8, Ex_19

---
+++ Komar_Mariia/Komar_Chapter_8,Ex_19garden path.py
@@ -0,0 +1,24 @@
+#Komar Mariia Als-11
+#Chapter_8, Ex_19
+
+#Read up on Уgarden pathФ sentences. How might the computational work of
+#a parser relate to the difficulty humans have with processing these sentences?
+#(See http://en.wikipedia.org/wiki/Garden_path_sentence.)
+
+question1=('Question:')
+print question1
+question=(' Read up on Уgarden pathФ sentences. How might the computational work of a parser relate to the difficulty humans have with processing these sentences? (See http://en.wikipedia.org/wiki/Garden_path_sentence.)')
+print question
+answer1=('Answer:')
+print answer1
+answer=('#Уgarden pathФ sentences - це граматично правильн≥ реченн€, в €ких кожне слово в≥доме, проте зрозум≥ти чи зробити cинтаксичний анал≥з таких речень важко €к ≥ дл€ людини, так ≥ дл€ комютера.Ќаприклад  УThe old man the boat.Ф це може означати, що "старий чолов≥к ... човен", табто реченн€ некоректне тому що без д≥Їслова,але насправд≥ "man" це д≥Їслово що означаЇ "укомплектовувати" ≥ це реченн€ перекладають €к "—тарий укомплектовуЇ човен". ќтже, €к ≥ людина так ≥ машина може припуститис€ помилки,хоча людина правильн≥ше анал≥зуЇ реченн€.')
+print answer
+
+#Уgarden pathФ sentences - це граматично правильн≥ реченн€, в €ких кожне слово
+#в≥доме, проте зрозум≥ти чи зробити cинтаксичний анал≥з таких речень важко
+#€к ≥ дл€ людини, так ≥ дл€ комютера. 
+#Ќаприклад  УThe old man the boat.Ф це може означати,
+#що "старий чолов≥к ... човен", табто реченн€ некоректне тому що без д≥Їслова,
+#але насправд≥ "man" це д≥Їслово що означаЇ "укомплектовувати" ≥ це реченн€
+#перекладають €к "—тарий укомплектовуЇ човен". ќтже, €к ≥ людина так ≥ машина
+#може припуститис€ помилки,хоча людина правильн≥ше анал≥зуЇ реченн€.
