d389f2f0032d3e70acc49a03aecaa51da76164da
ulianachubko <adamant1991@rambler.ru>
2013-04-25 22:31:35 +0300

    Vezdenko, chapter 5, 6,7

---
+++ Vezdenko_Chapter7_Ex1.py
@@ -0,0 +1,2 @@
+#Vezdenko##
+IOB format are nacessary because it provides mothods for readingand writting trees in Python. If we use only I and O, it will caused problem bacause Tag B marks the beginning of the chunk. so we could have errors.
