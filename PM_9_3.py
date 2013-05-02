a46ad1387a7b3d44aeb6186a5be837ba19e484ad
Pavutynka <marichka.pelekh@gmail.com>
2013-05-02 18:49:58 +0300

    9_3

---
+++ PM_9_3.py
@@ -0,0 +1,12 @@
+import nltk
+fs1 = nltk.FeatStruct("[A = [D = d]]")
+fs2 = nltk.FeatStruct("[A = [D = d], C = [F = [D = d]]]")
+fs1.subsumes(fs2)
+True
+def subs():
+	if fs1.subsumes(fs2):
+		print fs1.unify(fs2)
+	elif fs2.subsumes(fs1):
+		print fs1.unify(fs2)
+	else:
+		print "This two structures are not incommensurable"
