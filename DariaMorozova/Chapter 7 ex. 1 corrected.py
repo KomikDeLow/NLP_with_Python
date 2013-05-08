84e92a62418a12b52393cf522d651e71291205f5
Дарія Морозова <dashysik.nirvana@gmail.com>
2013-05-08 17:12:58 +0300

    Chapter 7 Ex. 1 corrected

---
+++ DariaMorozova/Chapter 7 ex. 1 corrected.py
@@ -0,0 +1,6 @@
+# Daria Morozova
+# AL-12
+# Chapter 7 Ex.1
+
+# In IOB tags' scheme each token is tagged with one of three special chunk tags, I(inside), O(outside), B(begin). IOB tags' scheme  is necessary for
+#creating chunks. And if we use I and O tags only we won't be able to find the beginning of our chunk. Because tag B is the tag of the beginning of a chunk.
