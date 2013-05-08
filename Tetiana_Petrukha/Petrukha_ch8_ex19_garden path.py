21c2717d547761081af1e376fdc0c8084391af18
Tetiana-Petrukha <taniapetrukha@gmail.com>
2013-05-08 21:30:02 +0300

    chapter 8 ex19

---
+++ Tetiana_Petrukha/Petrukha_ch8_ex19_garden path.py
@@ -0,0 +1,37 @@
+# Petrukha Tetiana Als-11
+# Chapter_8, Ex_19
+
+
+# Read up on “garden path” sentences. How might the computational work of
+# a parser relate to the difficulty humans have with processing these sentences?
+# (See http://en.wikipedia.org/wiki/Garden_path_sentence.)
+
+question1=('Question:')
+print question1
+question=(' Read up on “garden path” sentences. How might the computational work of a parser relate to the difficulty humans have with processing these sentences? (See http://en.wikipedia.org/wiki/Garden_path_sentence.)')
+print question
+print ("-------------------------------------------------------------------------------")
+answer1=('Answer:')
+print answer1
+answer=(' Garden path sentences - це граматично правильні речення, які починаються в такий спосіб, що в читачів тлумачення буде неправильним; Приклади "garden path sentence"'
+        '1) The author wrote the novel was likely to be a best-seller. The author composed the novel... or  The author wrote that the novel was likely to be a best-seller.'
+'2) The man returned to his house was happy. The man came back to his house... or Returned to his house, the man was happy.'
+'3) The government plans to raise taxes were defeated. The government is planning to raise taxes... or The governments plans to raise taxes were defeated.'
+'Синтаксичний аналіз визначає як діляться фрази в "garden path sentences". Синтаксичний аналіз є загальним терміном, який використовується в психолінгвістиці описуючи розуміння мови.'
+'Людина скоріше(правильніше), ніж комп’ютер аналізує речення (визначає частини мови, синтаксичні зв’язки  і т.д.)'
+'Синтаксичний аналізатор, так як і людина визначає як поділити речення , але до одного і того ж речення можливо приписувати різні граматичні структури.'
+'Наприклад  “He ate the cookies on the couch,” це може означати, що він їсть печиво яке є на кушетці, або, що він сидить на кушетці в той час коли їсть печиво.'
+'Отже, як і людина так і машина може припуститися помилки. ')
+print answer
+
+# "Garden path sentences" - це граматично правильні речення, які починаються
+# в такий спосіб, що в читачів тлумачення буде неправильним;
+# Приклади "garden path sentence":
+# 1) The author wrote the novel was likely to be a best-seller. The author composed the novel... or  The author wrote that the novel was likely to be a best-seller.
+# 2) The man returned to his house was happy. The man came back to his house... or Returned to his house, the man was happy.
+# 3) The government plans to raise taxes were defeated. The government is planning to raise taxes... or The government's plans to raise taxes were defeated.
+# Синтаксичний аналіз визначає як діляться фрази в "garden path sentences". Синтаксичний аналіз є загальним терміном, який використовується в психолінгвістиці описуючи розуміння мови.
+# Людина скоріше(правильніше), ніж комп’ютер аналізує речення (визначає частини мови, синтаксичні зв’язки  і т.д.)
+# Синтаксичний аналізатор, так як і людина визначає як поділити речення , але до одного і того ж речення можливо приписувати різні граматичні структури.
+# Наприклад  “He ate the cookies on the couch,” це може означати, що він їсть печиво яке є на кушетці, або, що він сидить на кушетці в той час коли їсть печиво.
+# Отже, як і людина так і машина може припуститися помилки.
