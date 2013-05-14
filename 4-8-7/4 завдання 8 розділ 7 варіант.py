6a6c8dd9c0bc0b3c416f3a449c205c953e1bcde5
zakala <marija.zakala@gmail.com>
2013-05-14 10:11:59 +0300

    Update 4 завдання 8 розділ 7 варіант.py

--- 4-8-7/4 завдання 8 розділ 7 варіант.py
+++ 4-8-7/4 завдання 8 розділ 7 варіант.py
@@ -7,21 +7,21 @@
 import nltk
 from nltk.corpus import treebank
 parsed_sents = treebank.parsed_sents()
-parsed_sents[4]
-print parsed_sents[4]
-type(parsed_sents[4])
+parsed_sents[3]
+print parsed_sents[3]
+type(parsed_sents[3])
 from nltk import Tree
 help(Tree)
- parsed_sents[4].draw()
-parsed_sents[4][0][0]
-parsed_sents[4][0][0]
-parsed_sents[4].node
-for i in parsed_sents[4]:i.node 
-for i in parsed_sents[4]:i.leaves
+ parsed_sents[3].draw()
+parsed_sents[3][0][0]
+parsed_sents[3][0][0]
+parsed_sents[3].node
+for i in parsed_sents[3]:i.node 
+for i in parsed_sents[3]:i.leaves
 print i.leaves
-for i in parsed_sents[4].productions():
+for i in parsed_sents[3].productions():
   print i# print rules for grammar
-for i in parsed_sents[4].productions():
+for i in parsed_sents[3].productions():
 	print i.rhs(), i.lhs()
-for i in parsed_sents[4].productions():
+for i in parsed_sents[3].productions():
 	print i.is_nonlexical
