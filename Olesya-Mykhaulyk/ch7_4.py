75524d3456518b63cb9a8f89367455043f5e06d6
Andriy Romanyuk <anrom7@gmail.com>
2013-05-15 11:16:15 +0300

    todo

--- Olesya-Mykhaulyk/ch7_4.py
+++ Olesya-Mykhaulyk/ch7_4.py
@@ -5,17 +5,17 @@
 #Olesya Mykhaulyk
 #ALs-12
 # Chapter 7 Ex.4
-#import nltk
+import nltk
 # sentence=[('Mary','NN'),('saw','VBD'),('the','DT'),('cat','NN'),('sit','VB'),('on','IN'),('the','DT'),('mat','NN')]
-#grammar=r"""
-#      NP:{<DT|JJ|NN.*>+} // chunk sequences of DT,JJ,NN
-#      PP:{<IN><NP>} // chunk prepositions followed by NP
-#      VP:{<VB.*><NP|PP|CLAUSE>+$}// chunk verbs and their arguments
-#      CLAUSE:{<NP><VP>}//chunk NP, VP
-#     """
+grammar=r"""
+      NP:{<DT|JJ|NN.*>+} // chunk sequences of DT,JJ,NN
+      PP:{<IN><NP>} // chunk prepositions followed by NP
+      VP:{<VB.*><NP|PP|CLAUSE>+$}// chunk verbs and their arguments
+      CLAUSE:{<NP><VP>}//chunk NP, VP
+"""
 
-# cp=nltk.RegexpParser(grammar)// testing created chunk parsel on the sentence
-# result = cp.parse(sentence)// print result
-# print result
+cp=nltk.RegexpParser(grammar) # testing created chunk parsel on the sentence
+result = cp.parse(sentence) # print result
+print result
 
 #also we can display the result graphically by using the result.draw() option
