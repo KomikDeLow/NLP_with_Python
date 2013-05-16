eb53e992783e8cdd4ce598b2c5338ca0408e7bf3
olyafuyarchuk <ofuyarchuk@mail.ru>
2013-05-16 19:46:35 +0300

    Chapter 9 task3

---
+++ Fuyarchuk/Fuyarchuk O. Chapter 9 Task 3.py
@@ -0,0 +1,14 @@
+# Fuyarchuk O/ Chapter 9 Task 3
+import nltk
+fs1=nltk.FeatStruct("[C =?x,A = [B = ?x]]")
+fs2=nltk.FeatStruct("[D =?x,C =?x,A = [B = ?x]]")
+print fs1.subsumes (fs2)# Визначаємо чи належить структура характеристик fs1 структурі fs2
+print fs2.subsumes (fs1)# Визначаємо чи належить структура характеристик fs2 структурі fs1
+def subs (fs1, fs2): # функція, яка обєднує інформацію двох структур характеристик
+                    #у випадку коли fs1 належить fs2
+    if fs1.subsumes (fs2):
+        print fs1.unify(fs2)
+    elif fs2.subsumes (fs1):
+        print fs2.unify(fs1)
+    else: print "None"
+print subs(fs1,fs2)
