78099941482f376cc13c0d490f1053e3fbda3c7d
Iryna <brukailo@ukr.net>
2013-05-16 22:56:20 +0300

    Chapter 5

--- Iryna Brykailo/Завдання 21.py
+++ Iryna Brykailo/Завдання 21.py
@@ -14,6 +14,7 @@ for (wd, tg) in nltk.corpus.brown.tagged_words():
             if tg[:2] == 'QL':
                 fd.inc(wd)
                 verbs=['adore', 'love', 'like', 'prefer']
+                nouns=['love', 'like']
                 for i in range(len(text)):
                     if text[i]in fd.keys()and text[i+1]in verbs and (text[i]+' '+text[i+1]) not in dic:
                         dic.append(text[i]+' '+text[i+1])
@@ -22,4 +23,5 @@ for (wd, tg) in nltk.corpus.brown.tagged_words():
 #У результаті виконання програми ми отримуємо ряд специфікаторів,
 #які найчастіше зустрічаються перед словами adore, love, like та prefer.
 #Тaкож можна зробити висновок, що ці слова також можуть викорстовуватися
-#не лише як дієслова, залежно від контексту.
+#не лише як дієслова, залежно від контексту, а і як іменники, та інші, наприклад прислівники.
+
