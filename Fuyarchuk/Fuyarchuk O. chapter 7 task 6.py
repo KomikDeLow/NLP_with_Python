99dfd880b75d9335580878ca7b50a5c99acf4c48
olyafuyarchuk <ofuyarchuk@mail.ru>
2013-05-16 19:41:31 +0300

    виправлене
    7.6

--- Fuyarchuk/Fuyarchuk O. chapter 7 task 6.py
+++ Fuyarchuk/Fuyarchuk O. chapter 7 task 6.py
@@ -4,12 +4,10 @@
 
 # Fuyarchuk O. Chapter 7 Task 6
 import nltk
-sentence=[ ("July", "NNP"), ("and", "CC"), ("August", "NNP"),("all", "DT"),
-           ("your", "PRP$"),("managers", "NNS"),("and","CC"),
-           ("supervisors","NNS"),("company","NN"), ("courts","NNS"),("and","CC"),
-           ("adjudicators","NNS")]
-grammar = "NP:{<DT>?<PRP$>?<NN.*>+<CC><NN.*>*}" #define grammar with
-                                                #regular expression rules
+sentence = [("July", "NNP"), ("and", "CC"), ("August", "NNP"), ("all", "DT"), ("your", "PRP$"),
+            ("managers", "NNS"), ("and", "CC"), ("supervisors", "NNS"), ("company", "NN"),
+            ("courts", "NNS"), ("and", "CC"), ("adjudicators", "NNS")] #Sentence that should be chunked.
+grammar = "NP:{<DT|PRP\$|NN.*>+<CC><NN.*>+}" #Define a grammar with the regular expression rules
 cp=nltk.RegexpParser(grammar)#using grammar create a chunk parser and test
 print cp.parse(sentence)
 from nltk.corpus import conll2000 # import conll2000 corpus
