795ae9f6ef38937db382d248aa3e6b23b8d0ac89
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-05-17 17:13:07 +0300

    Chapter 5  Ex 14

---
+++ Olesya-Mykhaulyk/5_14.py
@@ -0,0 +1,14 @@
+#TODO
+#Program is not well
+#You use a lot variables (words_brown,tage_brown,tage_brown,sortyem,bez_dyblikativ) and it is a bad programming style
+#You append tags to not empty tage_brown list
+#Olesya Mykhaulyk ALs-12 5.10-14
+#TODO
+#What does your loop do?
+import nltk
+from nltk.corpus import brown
+words_brown=brown.tagged_words()
+tage_brown=[t for w,t in words_brown]
+	
+set(sorted(tage_brown))
+
