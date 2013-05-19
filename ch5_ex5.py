9bc94dd310ed3535781e76f0b37a400c7d42b2f0
Iryna <18Sonce-Zaia18@i.ua>
2013-05-19 23:26:06 +0300

    Update ch5_ex5.py

--- ch5_ex5.py
+++ ch5_ex5.py
@@ -1,3 +1,6 @@
+#presented by Iryna Havryliak PRLc 13
+# Chapter 9, Ex.2
+
 import nltk
 d = {'chair' : 'table', 'window' : 'door'} #dictionary d, and add some entries
 d['flower'] = 'rose' #add new value
