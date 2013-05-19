758da28004b7001dbf30f0534498f64c28ea0c0e
Julia <Tsymbalista>
2013-05-19 14:55:26 +0300

    chapter_7_ex_8

---
+++ chapter_7_ex_8.py
@@ -0,0 +1,24 @@
+
+#Presented by Iuliia Tsymbalista
+
+#Chapter 7, ex. 8
+
+#Develop a chunker for one of the chunk types in the CoNLL Chunking Corpus
+#using a regular expression–based chunk grammar RegexpChunk.
+
+
+
+
+
+import nltk
+from nltk import *
+sentence=nltk.corpus.conll2000.chunked_sents('train.txt', chunk_types=('NP',))[99]
+grammar=r"NP:{<[CDJNP].*>+}" #opusujy gramatuky, jaka bude vydiliaty fragmenty
+#na osnovi reguliarnogo vyrazu jakyj opysue typovuj imennukovyj
+#vuraz (poslidovnist tegiv CD J NP)
+
+cp=nltk.RegexpParser(grammar)
+result = cp.parse(sentence)
+print sentence
+print result
+result.draw()
