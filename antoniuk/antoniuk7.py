1c5b4d3520d7fe4acd64f4d014cbc3d35826ef90
Alexandr Antoniuk <antonyuk.aleksandr@mail.ru>
2013-05-19 16:16:56 +0300

    виправлення

--- antoniuk/antoniuk7.py
+++ antoniuk/antoniuk7.py
@@ -5,8 +5,8 @@
 
 import nltk
 from nltk import*
-d1={}.fromkeys(['help', 'room', 'red'])
-d2={}.fromkeys(['phone', 'ask', 'clean', 'member'])
+d1={}.fromkeys(['help', 'room', 'red']) # словник1
+d2={}.fromkeys(['phone', 'ask', 'clean', 'member']) # словник2
 d1['help']='V'
 d1['room']='N'
 d1['red']='A'
@@ -14,7 +14,7 @@ d2['phone']='N'
 d2['ask']='V'
 d2['clean']='A'
 d2['member']='N'
-d1.update(d2)
+d1.update(d2) # оновлення словника
 d1
 print d1
 # dodae dani d2 do slovnuka d1
