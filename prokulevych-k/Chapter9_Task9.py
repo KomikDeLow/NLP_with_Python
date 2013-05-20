ba1f00868bd2dcd8c127f56fdc23a9b0e3ecddcc
Prokulevych K. <qqqkate@mail.ru>
2013-05-20 17:56:17 +0300

    Chapter9_Task9

---
+++ prokulevych-k/Chapter9_Task9.py
@@ -0,0 +1,10 @@
+#Прокулевич К. ПРЛс-11, Розділ9, Задача 9
+#List two feature structures that subsume [A=?x, B=?x].
+import nltk
+#giving values to features:
+fs1 = nltk.FeatStruct ("[A = ?x, B = ?x]")
+fs2 = nltk.FeatStruct ("[B = D]")
+print fs1.unify(fs2)
+fs1 = nltk.FeatStruct ("[A = ?x, B = ?x]")
+fs2 = nltk.FeatStruct ("[A = [ B= d]]")
+print fs1.unify(fs2)
