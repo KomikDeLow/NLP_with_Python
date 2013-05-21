c46a511e9e758fa797b38687637cf4b042377470
Mariia Komar <komar.mariia>
2013-05-21 20:43:07 +0300

    Chapter_9, Ex_10

---
+++ Komar_Mariia/Komar_Chapter_9,Ex_10.py
@@ -0,0 +1,10 @@
+#Komar Mariia, ALs-11
+#Chapter_9, Ex_10
+
+#Ignoring structure sharing, give an informal algorithm for unifying two feature
+#structures.
+
+import nltk
+fs1=nltk.FeatStruct(COUNTRY='UKRAINE', CITY='LVIV', UNIVERSITY='LVIV POLYTECHNIK') #створюємо першу структуру ознак
+fs2=nltk.FeatStruct(ACADEMIC_INSTITUTES='ICSIT', DEPARTMENT='APLIED LINGUISTC')#створюємо другу структуру ознак
+print fs1.unify(fs2) #здійснюємо операцію уніфікації
