b58a7e7f79209197fd7a712de2c355f561aaab77
Mariia Komar <komar.mariia>
2013-05-21 19:47:51 +0300

    Chapter10_Ex_4

---
+++ Komar_Mariia/Komar_Chapter10_Ex4.py
@@ -0,0 +1,39 @@
+﻿# Komar Mariia ALs-11
+# Chapter_10, Ex_4
+
+#Translate the following verb phrases using λ-abstracts and quantified formulas of first-order logic.
+
+#a. feed Cyril and give a capuccino to Angus
+#∀x.(feed(x,Cyril) ^ give(y,capuccino))
+#λxλy.(feed(x) & give(y))
+print 'sentence_A  =  feed Cyril and give a capuccino to Angus'
+print 'λ-abstracts formulas of first-order logic =  λxλy.(feed(x) & give(y))'
+print '-------------------------------------------------------------------------'
+
+#b. be given ‘War and Peace’ by Pat
+#∃x.(give(x,‘War and Peace’) ^ giveer(y,Pat))
+#λxλy.(give(x) & giver(y))
+print 'sentence_A  =  feed Cyril and give a capuccino to Angus'
+print 'λ-abstracts formulas of first-order logic =  λxλy.(give(x) & giver(y))'
+print '-------------------------------------------------------------------------'
+
+#c. be loved by everyone
+#∀x.(love(x) ^ lover(y,everyone))
+#λxλy.(love(x) ^ lover(y))
+print 'sentence_A  =  feed Cyril and give a capuccino to Angus'
+print 'λ-abstracts formulas of first-order logic =  λxλy.(love(x) ^ lover(y))'
+print '-------------------------------------------------------------------------'
+
+#d. be loved or detested by everyone
+#∀x.((love(x) ^ lover(y,everyone))|( detest(x) ^ detester(y,everyone))
+#λxλy.((love(x) ^ lover(y))|( detest(x) ^ detester(y))
+print 'sentence_A  =  feed Cyril and give a capuccino to Angus'
+print 'λ-abstracts formulas of first-order logic =  λxλy.((love(x) ^ lover(y))|( detest(x) ^ detester(y))'
+print '-------------------------------------------------------------------------'
+
+#e. be loved by everyone and detested by no-one
+#∀x.((love(x) ^ lover(y,everyone))^( detest(x) ^ detester(z,no-one))
+#λxλyλz.((love(x) ^ lover(y))^( detest(x) ^ detester(z))
+print 'sentence_A  =  feed Cyril and give a capuccino to Angus'
+print 'λ-abstracts formulas of first-order logic =  λxλyλz.((love(x) ^ lover(y))^( detest(x) ^ detester(z))'
+print '-------------------------------------------------------------------------'
