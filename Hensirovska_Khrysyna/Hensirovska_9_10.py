2f2ec0b17c8a4981cd1dba7f52e5e34bca1e87de
Khrystyna <xrustja_gensirovska@ukr.net>
2013-05-21 16:43:26 +0300

    Chapter9,task 10

---
+++ Hensirovska_Khrysyna/Hensirovska_9_10.py
@@ -0,0 +1,11 @@
+#Khrystyna Hensirovska,ALs-12
+#Chapter 9,task 10
+#Ignoring structure sharing, give an informal algorithm for unifying two feature
+#structures.
+mport nltk
+#encode information about a hotel in a feature structure
+fs1=nltk.FeatStruct(CITY='Ivano-Frankivsk',STREET='Ivana Franka',HOTEL='Nadia')
+#exploring feature structure
+fs2=nltk.FeatStruct(stars='three',rooms='one hundred')
+print fs2.unify(fs1) #Merging information from two feature structures is called unification and is supported
+#by the unify() method.
