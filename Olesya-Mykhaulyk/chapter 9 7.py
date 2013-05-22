102a2d694ef525c869899d3ea20931dc2c846435
Olesya Mykhaulyk <olesya_m@hotmail.com>
2013-05-22 22:35:06 +0300

    chapter 9 ex 7

---
+++ Olesya-Mykhaulyk/chapter 9 7.py
@@ -0,0 +1,9 @@
+﻿#Olesya Mykhaulyk
+#ALs-12
+#Chapter 9 ex7 
+import nltk 
+from nltk import load_parser 
+tokens = 'ich folge den Katze'.split()
+cp = load_parser('grammars/book_grammars/german.fcfg', trace=2) #returns a chart parser 
+trees = cp.nbest_parse(tokens) #Calling the parser�s nbest_parse() method 
+#will return a list trees of parse trees
