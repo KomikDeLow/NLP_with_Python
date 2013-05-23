9987de5a386cf81ccc0bc844b767f439a9a3ddd7
Oksana <1blackrose@ukr.net>
2013-05-23 17:42:16 +0300

    added 7.1

---
+++ Oksana_Pokladok/Pokladok_7_zavd_1.py
@@ -0,0 +1,23 @@
+import nltk
+# we add a text (information in a file)
+text='''
+We PRP B-NP
+saw VBD O
+the DT B-NP
+little JJ I-NP
+cat NN I-NP
+'''
+nltk.chunk.conllstr2tree(text, chunk_types=['NP']).draw() # building a tree representation from the string
+# text only with I and O tags 
+text='''
+cat NN I-NP
+. . O
+'''
+nltk.chunk.conllstr2tree(text, chunk_types=['NP']).draw() # building a tree representation from the string
+
+# The most widespread file representation uses IOB tags. In this scheme, each token is tagged with one of three special
+# chunk tags, I (inside), O (outside), or B (begin). A token is tagged as B if it marks the
+# beginning of a chunk. Subsequent tokens within the chunk are tagged I. All other
+# tokens are tagged O. The B and I tags are suffixed with the chunk type, e.g., B-NP, INP.
+# Of course, it is not necessary to specify a chunk type for tokens that appear outside
+# a chunk, so these are just labeled O.
