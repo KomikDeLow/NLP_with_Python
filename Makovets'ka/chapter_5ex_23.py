6d5e6939471aa20c62476fdc813814885d767ade
Yulia Makovets'ka (Юля Маковецька) <m_yulichka@mail.ru>
2013-05-23 21:18:08 +0300

    chapter_5
    ex_23

---
+++ Makovets'ka/chapter_5ex_23.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.corpus import brown
+patterns = [
+    (r'.*ing$', 'VBG'), 
+    (r'.*ed$', 'VBD'),
+     (r'.*ould$', 'MD'),
+    (r'.*es$', 'VBZ'),
+    (r'.*s$', 'NNS'),
+    (r'.*\'s$', 'NN$'),
+    (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+    (r'.*', 'NN')
+    ]
+regexp_tagger = nltk.RegexpTagger(patterns)
+regexp_tagger.tag(brown.sents()[10])
+print regexp_tagger.tag(brown.sents()[10])
+brown_test=brown.tagged_sents()[:50]
+100.0 * regexp_tagger.evaluate(brown.tagged_sents()[50:500])
+print 100.0 * regexp_tagger.evaluate(brown.tagged_sents()[50:500])
+two_tagger=nltk.UnigramTagger(brown_test, backoff=regexp_tagger)
+100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500])
+print 100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500])
