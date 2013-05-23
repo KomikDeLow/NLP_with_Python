b4cc1609eb236f2103c317d4c1c604d0962bed1d
Yulia Makovets'ka (Юля Маковецька) <m_yulichka@mail.ru>
2013-05-23 21:24:03 +0300

    chap_5
    ex_23

--- Makovets'ka/chapter_5ex_23.py
+++ Makovets'ka/chapter_5ex_23.py
@@ -1,10 +1,12 @@
+#work with tagger
+#improve its performance
 import nltk
 from nltk.corpus import brown
 patterns = [
-    (r'.*ing$', 'VBG'), 
-    (r'.*ed$', 'VBD'),
-     (r'.*ould$', 'MD'),
+    (r'.*ing$', 'VBG'),
     (r'.*es$', 'VBZ'),
+    (r'.*ed$', 'VBD'),
+    (r'.*ould$', 'MD'),
     (r'.*s$', 'NNS'),
     (r'.*\'s$', 'NN$'),
     (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
@@ -19,3 +21,5 @@ print 100.0 * regexp_tagger.evaluate(brown.tagged_sents()[50:500])
 two_tagger=nltk.UnigramTagger(brown_test, backoff=regexp_tagger)
 100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500])
 print 100.0 * two_tagger.evaluate(brown.tagged_sents()[50:500])
+
+         
