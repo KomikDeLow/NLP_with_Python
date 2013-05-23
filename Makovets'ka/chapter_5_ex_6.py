b6ff9be2f5becdf137ae98dcd7f41cfeaa9fb382
Yulia Makovets'ka (Юля Маковецька) <m_yulichka@mail.ru>
2013-05-23 21:41:26 +0300

    chapter_5
    ex_6

---
+++ Makovets'ka/chapter_5_ex_6.py
@@ -0,0 +1,12 @@
+#Try deleting an element
+#from a dictionary d,
+#using the syntax del
+#d['abc']
+#Check that the item
+#was deleted
+pos={}
+pos={'beautiful': 'ADJ','girl':'N','sings':'V','well':'ADV'}
+print pos
+del pos['sings']
+print 'Deleted entry (sings)'
+print pos
