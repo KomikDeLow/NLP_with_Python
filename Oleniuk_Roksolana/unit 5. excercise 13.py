4db3a71f58bb59a6e6c124c768e3e2e402eefb42
Roksolana <ms.oleniuk@list.ru>
2013-05-23 14:12:49 +0300

    задачі 5 юніт

---
+++ Oleniuk_Roksolana/unit 5. excercise 13.py
@@ -0,0 +1,9 @@
+# unit 5 Excercise 13
+>>> import datetime
+>>> today = datetime.date.today()
+>>> print today
+
+>>> mylist = []
+>>> today = datetime.date.today()
+>>> mylist.append(today)
+>>> print mylist
