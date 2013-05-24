346ee3a5fc2cda034baa21b8cb1cbb77e8c2da5d
Kseniya <K.shylimova@gmail.com>
2013-05-24 20:32:23 +0300

    розділ 7, завд 5

---
+++ Shylimova/Shylimova_7_5.py
@@ -0,0 +1,23 @@
+# Shylimova K. Al-13
+# Chapter 7, task 5
+
+import nltk
+from nltk import RegexpParser
+
+grammar = r"""
+    NP: {<DT>?<VBG>?<NN>}
+    NP: {<NN>?<VBG>?<NN>}
+    NP: {<VBG>?<NN>}
+    NP: {<NN>?<VBG>}"""
+sentences = [[("The","DT"),("working","VBG"),("day","NN")],
+             [("The","DT"),("Developing","VBG"),("Economies","NN")],
+             [("The","DT"),("fighting","VBG"),("machine","NN")],
+             [("Horse","NN"),("riding","VBG"),("rules","NN")],
+             [("Air","NN"),("conditioning","VBG"),("systems","NN")],
+             [("Freedom","NN"),("loving","VBG"),("people","NN")],
+             [("Dying","VBG"),("swan","NN")],
+             [("Deer","NN"),("hunting","VBG")],]
+
+parser = RegexpParser(grammar)
+for sent in sentences:
+    print parser.parse(sent)
