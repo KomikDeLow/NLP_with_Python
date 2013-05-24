db0849c6475c32a90c49263f2f744efb1c1bc098
Veronika <Nika_gold@i.ua>
2013-05-25 00:03:19 +0400

    Chapter 5_32 vupravlena

---
+++ Klantsak/Klantsak_Chapter_5_32vupravlene.py
@@ -0,0 +1,14 @@
+﻿# TODO
+# Коментарі! Що програма робить?
+# Потрібно провести декілька експериментів з різними значеннями аргументів
+
+# Veronika KLantsak ALs-11
+#  Experiment with the tagger by setting different values
+# for the parameters. Is there any trade-off between training time (corpus size) and
+# performance?
+import nltk
+help(nltk.tag.brill.demo)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.4, trace=4)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.6, trace=4)
+nltk.tag.brill.demo(num_sents=2000, max_rules=200, min_score=3, error_output='errors.out', rule_output='rules.yaml', randomize=False, train=0.9, trace=4)
+# With the increase in size of the corpus to process slows the implementation process
