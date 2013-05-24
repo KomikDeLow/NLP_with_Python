a6fbe6f55f234a92eb38df50767d5c2339d14b9e
Veronika <Nika_gold@i.ua>
2013-05-25 00:19:50 +0400

    Chapter_5_9 vupravlene

---
+++ Klantsak/Klantsak_Chapter_5_9vupravlene.py
@@ -0,0 +1,14 @@
+﻿# TODO
+# Модуль не містить коментарів
+
+# Veronika KLantsal ALs-11
+#
+import nltk
+from nltk.corpus import brown
+text1 = nltk.word_tokenize("Go away!")#zrobula tokenizaciyu perwogo rechennya
+nltk.pos_tag(text1)# python vuznachae wo "Go" e imennukom y rechenni "Go away"
+# Ce nepravul'ne vuznachennya oskil'ku "Go" e dieslovo
+text2 = nltk.word_tokenize("We went on the excursion.")#tokenizaciya drygogo rechennya
+nltk.pos_tag(text2)# rezyl'tat: "went" dieslovo y munylomy chasi
+print  nltk.pos_tag(text1)
+print  nltk.pos_tag(text2)
