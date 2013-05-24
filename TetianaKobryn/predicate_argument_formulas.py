1a5c31da28aeff0864dbd9879096fb74bce151eb
Tetiana <kobryn.t@gmail.com>
2013-05-24 16:40:48 +0300

    new file

---
+++ TetianaKobryn/predicate_argument_formulas.py
@@ -0,0 +1,33 @@
+#Presented by Tetiana Kobryn
+#
+#Chapter 10, Ex.2
+#Translate sentences into predicate-argument formulas of first-order logic.
+
+
+"""a. Angus likes Cyril and Irene hates Cyril.
+like(Angus, Cyril) & hate(Irene, Cyril)
+"""
+
+
+"""b. Tofu is taller than Bertie.
+taller(Tofu, Bertie)"""
+
+
+"""c. Bruce loves himself and Pat does too.
+love(Bruce, Bruce) & love (Pat, Pat)
+"""
+
+"""
+d. Cyril saw Bertie, but Angus didn’t.
+saw(Cyril, Bertie) & -saw(Angus, Bertie)
+"""
+
+"""
+e. Cyril is a four-legged friend.
+is(C, f)
+"""
+
+"""
+f. Tofu and Olive are near each other.
+be_near(T, O) & be_near(O, T)
+"""
