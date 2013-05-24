f7a6328c4d404593cb888b842352c4802f828728
VeronNika <nika_gold@i.ua>
2013-05-25 01:35:47 +0400

    Update Klantsak_Chapter_5_15a.py

--- Klantsak/Klantsak_Chapter_5_15a.py
+++ Klantsak/Klantsak_Chapter_5_15a.py
@@ -6,13 +6,13 @@ import nltk
 from nltk.corpus import brown
 s=[]
 a=[]
-for (word, tag) in brown.tagged_words(categories='news'):
+for (word, tag) in brown.tagged_words(categories='news'): # powyk sliv yaki mayut' teg imennuka v odnuni
     if tag=='NN':
         s.append(word)
 
 
 
-fdist = nltk.FreqDist(s)
+fdist = nltk.FreqDist(s) #stvorennya chastotnogo spusky 
 for (word, tag) in brown.tagged_words(categories='news'):
     if tag=='NNS':
         a.append(word[:-1])
@@ -20,7 +20,7 @@ for (word, tag) in brown.tagged_words(categories='news'):
 
 
 fdist1 = nltk.FreqDist(a)
-for i in fdist.keys():
+for i in fdist.keys(): # porivnyannya chastotu vxodzhennya v spusku imennukiv.
     if fdist1[i]>fdist[i]:
         print i
         
