e837a1af9ae560c089bcac199ebd1fc3ae4ae83c
Veronika <Nika_gold@i.ua>
2013-05-25 02:16:23 +0400

    Chapter_8_23

---
+++ Klantsak/Klantsak_Chapter_8_23.py
@@ -0,0 +1,15 @@
+# Veronika Klantsak ALs-11
+
+# In  Section 8.2, we claimed that there are linguistic regularities that cannot be
+# described simply in terms of n-grams. Consider the following sentence, particularly
+# the position of the phrase  in his turn. Does this illustrate a problem for an approach
+# based on n-grams?
+
+# What was more, the in his turn somewhat youngish Nikolay Parfenovich also turned
+# out to be the only person in the entire world to acquire a sincere liking to our 
+# “discriminated-against” public procurator. Dostoevsky: The Brothers Karamazov)
+
+# Answer:
+
+# In English are sustainable word order but common literary language also applies indirect procedure
+# as an artistic means. This can cause problems for programm  for which such structures are unclear.
