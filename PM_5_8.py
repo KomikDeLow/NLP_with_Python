9724fc325eb6ef0458334602d1fe268cd961cc08
Pavutynka <marichka.pelekh@gmail.com>
2013-05-25 22:18:53 +0300

    5_8_added

--- PM_5_8.py
+++ PM_5_8.py
@@ -1,9 +1,7 @@
-# TODO
-# Comments?
-#
-
 import nltk
+# create a dictionary whose default value is list
 books=nltk.defaultdict(list)
+# create a dictionary which displays headword, part_of_speech, sense and example and define keys
 def mydic (headword, part_of_speech, sense, example):
 	books[headword]=[part_of_speech, sense, example]
 
@@ -12,6 +10,7 @@ mydic('magazine', 'N', ' publications that are printed with ink on paper, and ge
 mydic('novel', 'N', 'a long prose narrative that describes fictional characters and events in the form of a sequential story, usually', 'Don Quixote is the greatest novel of all time.')
 mydic ('comics', 'N', 'an artistic medium in which images incorporate text or other visual forms of information in order to express a narrative or idea', 'The 1938 success of Action Comics and its lead hero Superman marked the beginning of the Golden Age of comic books, in which the superhero genre was most prominent.')
 mydic('trilogy', 'N', 'a set of three works of art that are connected, and that can be seen either as a single work or as three individual works', 'Most trilogies are works of fiction involving the same characters or setting, such as The Deptford Trilogy.')
+# display results
 books['magazine']
 ['N', ' publications that are printed with ink on paper, and generally published on a regular schedule and containing a variety of content', 'The oldest consumer magazine still in print is The Scots Magazine, which was first published in 1739.']
 
