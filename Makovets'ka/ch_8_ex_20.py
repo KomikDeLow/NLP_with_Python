d105e7f6161ffdbabf5a81809b263783d922c281
Yulia Makovets'ka (Юля Маковецька) <m_yulichka@mail.ru>
2013-05-25 14:03:43 +0300

    chapter8
    ex_20

---
+++ Makovets'ka/ch_8_ex_20.py
@@ -0,0 +1,12 @@
+#To compare multiple trees in a single window, we can use the draw_trees()
+#method. Define some trees and try it out.
+
+from nltk.corpus import treebank
+from nltk.draw.tree import draw_trees
+# defining trees using  Penn Treebank Corpus
+tree1 = treebank.parsed_sents('wsj_0001.mrg')[0]
+tree2 = treebank.parsed_sents('wsj_0002.mrg')[0]
+tree3 = treebank.parsed_sents('wsj_0003.mrg')[4]
+#presenting these trees, drawing them
+draw_trees(tree1, tree2, tree3)
+
