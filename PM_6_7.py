af6ffe163813ad261db953b48afc4838cd323277
Pavutynka <marichka.pelekh@gmail.com>
2013-05-25 22:01:36 +0300

    6_7_added

--- PM_6_7.py
+++ PM_6_7.py
@@ -1,26 +1,17 @@
-# TODO
-# Comments?
-# What does your program do and how?
-
 import nltk
-posts=nltk.corpus.nps_chat.xml_posts()
-history=[]
-def dialogue_act_features(post,p,history):
+# call xml_posts() to get a data structure representing the XML annotation for each post
+posts=nltk.corpus.nps_chat.xml_posts()[:100]
+# define a simple feature extractor that checks what words the post contains
+def dialogue_act_features(post):
 	features={}
 	for word in nltk.word_tokenize(post):
 		features['contains(%s)' % word.lower()]=True
-		if p==0:
-			features["prev-class"]=""
-		else:
-			features["prev-class"]=history[p-1]
-	return features
-
-featuresets=[]
-for p, post in enumerate(posts):
-	featuresets.append((dialogue_act_features(post.text, p, history), post.get('class')))
-	history.append(post.get('class'))
-
+		return features
+# construct the training and testing data by applying the feature extractor toeach post using post.get('class') to get a post’s dialogue act type
+featuresets=[(dialogue_act_features(post.text), post.get('class')) for post in posts]
 size=int(len(featuresets)*0.1)
 train_set, test_set=featuresets[size:], featuresets[:size]
+# create a new classifier
 classifier=nltk.NaiveBayesClassifier.train(train_set)
+# calculate the accuracy of a classifier model on a given test set
 print nltk.classify.accuracy(classifier, test_set)
