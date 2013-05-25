1d5b7e9a22664b234cb185244dec5423d8b125ed
Diana <bahen@ukr.netr>
2013-05-25 11:09:38 +0300

    Chapter 9, Exercises 4,5,9,12

---
+++ Bahen/Bahen_9_zavd_12.py
@@ -0,0 +1,9 @@
+# Bahen Diana, PRLs-11, Chapter 9, Exercise 12
+
+S -> NP VP
+NP -> Det N
+VP -> V Det N P N | V N P Det N 
+Det -> 'The'| 'the'
+N -> 'farmer' | 'cart' | 'sand' 
+V -> 'loaded' | 'filled' | 'dumped'
+P -> 'into' | 'with'
---
+++ Bahen/Bahen_9_zavd_9.py
@@ -0,0 +1,10 @@
+# Bahen Diana, PRLs-11, Chapter 9, Exercise 9
+
+import nltk
+fs_main=nltk.FeatStruct("[A=?x,B=?x]")# main feature set
+# subsumed structures
+fs1=nltk.FeatStruct("[A=?x]")
+fs2=nltk.FeatStruct("[B=?x]")
+# cheking if subsumed structures are in main structure
+print 'fs1 subsumes fs_main: ',fs1.subsumes(fs_main)
+print 'fs2 subsumes fs_main: ', fs2.subsumes(fs_main)
