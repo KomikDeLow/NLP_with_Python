2fecd28e68f1e07f0b5c60c5fd031c48d97ce2f2
Olya Truniak <olha.truniak@gmail.com>
2013-05-25 17:08:10 +0300

    Ch. 9, Ex. 12

---
+++ Truniak O/Grammar production for verbs.py
@@ -0,0 +1,10 @@
+# Presented by Olha Truniak. ALs-13
+# Chapter 9, Ex.12
+
+# Seemingly synonymous verbs have slightly different syntactic properties (Levin,
+# 1993). Consider the following patterns of grammaticality for the verbs loaded,
+# filled, and dumped. Can you write grammar productions to handle such data?
+
+import nltk
+nltk.data.show_cfg('grammars/book_grammars/featmodified.fcfg')
+""" I created new grammar productions, namely: rules for these sentences"""
