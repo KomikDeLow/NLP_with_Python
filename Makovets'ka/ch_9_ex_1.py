2d7318be31fa6a49867cdd146e4fe613d5d21f0b
Yulia Makovets'ka (Юля Маковецька) <m_yulichka@mail.ru>
2013-05-25 13:18:25 +0300

    chapter_9
    ex_1

---
+++ Makovets'ka/ch_9_ex_1.py
@@ -0,0 +1,18 @@
+#What constraints are required to correctly parse word sequences like
+#am happy and she is happy but not
+#*you is happy or *they am happy? 
+
+import nltk
+grammar1 = nltk.parse_cfg("""
+S -> NP_SG VP_1st_SG
+S -> NP_SG VP_3rd_SG
+NP_SG -> Pro
+VP_1st_SG -> Cop_1st_SG_Adj
+VP_3rd_SG -> Cop_3rd_SG_Adj
+Cop_1st_SG ->'am'
+Cop_3rd_SG ->'is'
+PRO -> 'I' | 'She'
+Adj -> 'happy'
+Cop -> 'is | am '
+""") #rules
+print grammar1
