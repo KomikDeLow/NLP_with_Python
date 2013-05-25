3c30dbedcf212cfa0f34514e3f4ac14a0478327c
Roksolana <Roksolana3012@ukr.net>
2013-05-25 23:10:38 +0300

    Ch10,Ch8

---
+++ Dyrda Roksolana/Dyrda ch10 ex2.py
@@ -0,0 +1,26 @@
+# Dyrda Roksolana AL-13
+# ch10, ex.2
+
+#Translate the following sentences into predicate-argument formulas of first-order
+#logic.
+
+
+# a)Angus likes Cyril and Irene hates Cyril.
+# 'like(Angus, Cyril) & hate(Irene, Cyril)'
+
+#b)Tofu is taller than Bertie.
+#'taller(Tofu, Bertie)'
+
+#c)Bruce loves himself and Pat does too.
+#'love(Bruce, Bruce) & love (Pat, Pat)'
+#this sentences can be writen in another way:
+# 'love(Bruce, Bruce) & love(Pat, Bruce)
+
+#d)Cyril saw Bertie, but Angus didn’t.
+#'saw(Cyril, Bertie) & -saw(Angus, Bertie)'
+
+#e)Cyril is a four-legged friend.
+#'is(Cyril, friend)'
+
+#f)Tofu and Olive are near each other.
+#'is_near(Tofu, Olive) & is_near(Olive, Tofu)'
---
+++ Dyrda Roksolana/Dyrda ch10 ex4.py
@@ -0,0 +1,43 @@
+# Dyrda R.
+
+# Ch10, Ex.4
+# Translate verb phrases using abstracts and quantified formulas
+# of first-order logic.
+
+import nltk
+lp = nltk.LogicParser()
+
+#a. feed Cyril and give a capuccino to Angus
+#\x.\y.\z.(feed(x) & give(y, z))(Cyril, capuccino, Angus)
+e = lp.parse(r'\x.\y.\z.(feed(x) & give(y, z))(Cyril, capuccino, Angus)')
+print 'example a:', e
+print e.simplify() 
+
+
+# b. be given ‘War and Peace’ by Pat
+#\x.\y.(be_given(x, y)) (War and Peace, Pat)
+e = lp.parse(r'\x.\y.(be_given(x, y)) (War_and_Peace, Pat)')
+print 'example b:', e
+print e.simplify() 
+
+
+#c. be loved by everyone
+#\x.(be_loved(x))(everyone)
+e = lp.parse(r'\x.(be_loved(x))(everyone)')
+print 'example c:', e
+print e.simplify() 
+
+
+#d. be loved or detested by everyone
+#\x.(be_loved(x) | be_detested(x))(everyone)
+e = lp.parse(r'\x.(be_loved(x) | be_detested(x))(everyone)')
+print 'example d:', e
+print e.simplify()
+
+
+#e. be loved by everyone and detested by no-one
+#\x.\y.(be_loved(x) & be_detested(y))(everyone)
+e = lp.parse(r'\x.(be_loved(x) & be_detested(x))(everyone)')
+print 'example e:', e
+print e.simplify()
+
