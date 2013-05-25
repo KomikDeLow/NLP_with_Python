4236870789217b6420e33b05d9b61136af2a38fe
Roksolana <ms.oleniuk@list.ru>
2013-05-25 22:35:34 +0300

    unit 5

--- Oleniuk_Roksolana/unit 5. excercise 13.py
+++
@@ -1,9 +0,0 @@
-# unit 5 Excercise 13
->>> import datetime
->>> today = datetime.date.today()
->>> print today
-
->>> mylist = []
->>> today = datetime.date.today()
->>> mylist.append(today)
->>> print mylist
