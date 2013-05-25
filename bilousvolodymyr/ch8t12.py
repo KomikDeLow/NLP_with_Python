00d50340a1f86d9ec02d4be34c6f003282210478
VovaTalib <bilous.volodymyr@gmail.com>
2013-05-25 00:02:24 -1200

    ch6 t3 t7 ch8 t12 t21 ch9 t8

---
+++ bilousvolodymyr/ch8t12.py
@@ -0,0 +1,8 @@
+# Bilous Volodymyr, AL-11
+#Chapter 8, Task 12
+#We have seen that a chart parser adds but never removes edges from a chart.
+#Why?
+
+#A chart parser hypothesizes constituents (i.e. adds edges) based on the grammar, the tokens, and the constituents already found.
+#Any constituent that is compatible with the current knowledge can be hypothesized.
+
