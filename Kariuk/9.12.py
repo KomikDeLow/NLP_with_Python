7458b2392eb52f1a79142044fcd50eb018821225
Roman Borys <r_borys@windowslive.com>
2013-05-25 23:58:26 +0300

    tasks

---
+++ Kariuk/9.12.py
@@ -0,0 +1,10 @@
+# Rostyslav Kariuk PRLs.11
+# Rozdil 9 zadacza 12
+
+S -> NP VP
+NP -> Det N
+VP -> V Det N P N | V N P Det N 
+Det -> 'The'| 'the'
+N -> 'farmer' | 'cart' | 'sand' 
+V -> 'loaded' | 'filled' | 'dumped'
+P -> 'into' | 'with'
---
+++ Kariuk/9.9.py
@@ -0,0 +1,11 @@
+# Rostyslav Kariuk PRLs.11
+# Rozdil 9 zadacza 9
+
+import nltk
+fs_main=nltk.FeatStruct("[A=?x,B=?x]")# main feature set
+# subsumed structures
+fs1=nltk.FeatStruct("[A=?x]")
+fs2=nltk.FeatStruct("[B=?x]")
+# cheking if subsumed structures are in main structure
+print 'fs1 subsumes fs_main: ',fs1.subsumes(fs_main)
+print 'fs2 subsumes fs_main: ', fs2.subsumes(fs_main)
