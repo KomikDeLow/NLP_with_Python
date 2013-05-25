b8d183d9fa09fb18ae93b884432a1b601b4d4f76
zakala <marija.zakala@gmail.com>
2013-05-25 23:39:11 +0400

    Update chapter 7, 5 ex.py

--- chapter 7, 5 ex.py
+++ chapter 7, 5 ex.py
@@ -1,17 +1,20 @@
-# TODO
-# It isn't the Python modul
-#
-#
+import nltk
+from nltk import RegexpParser
 
->>> import nltk
->>> grammar = r"""
-  NP: {<DT|NN>?<VBG><NN>}"""
->>> sentence = [("the", "DT"), ("receiving", "VBG"), ("end", "NN"),(",", ","),
-("assistant", "NN"), ("managing", "VBG"), ("editor", "NN")]
->>> cp = nltk.RegexpParser(grammar)
->>> print cp.parse(sentence)
-(S
-  (NP the/DT receiving/VBG end/NN)
-  ,/,
-  (NP assistant/NN managing/VBG editor/NN))
->>>
+grammar = r"""
+    NP: {<DT>?<VBG>?<NN>}
+    NP: {<NN>?<VBG>?<NN>}
+    NP: {<VBG>?<NN>}
+    NP: {<NN>?<VBG>}"""
+sentences = [[("The","DT"),("working","VBG"),("day","NN")],
+             [("The","DT"),("Developing","VBG"),("Economies","NN")],
+             [("The","DT"),("working","VBG"),("machine","NN")],
+             [("Horse","NN"),("riding","VBG"),("rules","NN")],
+             [("Air","NN"),("conditioning","VBG"),("systems","NN")],
+             [("Freedom","NN"),("liking","VBG"),("people","NN")],
+             [("Dying","VBG"),("swan","NN")],
+             [("Deer","NN"),("hunting","VBG")],]
+
+parser = RegexpParser(grammar)
+for sent in sentences:
+    print parser.parse(sent)
