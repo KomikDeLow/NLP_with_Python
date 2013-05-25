a2ab7f99ceff4efbe7abae407a2f6d71d7217eac
Pavutynka <marichka.pelekh@gmail.com>
2013-05-25 21:13:41 +0300

    6_4_new

--- PM_6_4.py
+++ PM_6_4.py
@@ -1,17 +1,31 @@
 import nltk
-# define a feature extractor that builds a dictionary containing relevant information about a given name Marichka
-def gender_features(word):
-	return{'last_letter':word[-1]}
-gender_features('Marichka')
-{'last_letter': 'a'}
-# prepare a list of examples and corresponding class labels
-from nltk.corpus import names
+# import Movie Reviews Corpus, which categorizes each review as positive or negative
+from nltk.corpus import movie_reviews
 import random
-names=([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
-random.shuffle(names)
-# use the feature extractor to process the names data, and divide the resulting list of feature sets into a training set and a test set
-featuresets=[(gender_features (p), m) for (p,m) in names]
-train_set, test_set=featuresets[200:], featuresets[:200]
+documents=[(list(movie_reviews.words(fileid)),category)
+	   for category in movie_reviews.categories()
+	   for fileid in movie_reviews.fileids(category)]
+random.shuffle(documents)
+# define a feature extractor for documents, so the classifier will know which aspects of the data it should pay attention to
+all_words=nltk.FreqDist(w.lower() for w in movie_reviews.words())
+word_features=all_words.keys()[:500]
+# define a feature extractor that simply checks whether each of these words is present in a given document
+def document_features(document):
+	document_words=set(document)
+	features={}
+	for word in word_features:
+		features['contains(%s)' %word]=(word in document_words)
+		return features # return features of the function to use it again
+# train and test our classifier for document classification
+featuresets= [(document_features (p), m) for (p,m) in documents]
+train_set, test_set=featuresets[100:], featuresets[:100]
+# build a classifier
 classifier=nltk.NaiveBayesClassifier.train(train_set)
-# examine the classifier to determine which features it found most effective for distinguishing the names’ genders 
+# compute its accuracy on the test set to check how reliable the resulting classifier is
+print nltk.classify.accuracy(classifier, test_set)
+0.49
+# use show_most_informative_features() to find out which features the classifier found to be most informative
 classifier.show_most_informative_features(30)
+
+
+
