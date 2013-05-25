b1354334b6afba882c1f7d498a6ff677f85834ec
Olya Truniak <olka115@mail.ru>
2013-05-25 21:03:01 +0300

    Update TrOlha_Ch. 8, Ex. 12.py
    update some expressions

--- Truniak O/TrOlha_Ch. 8, Ex. 12.py
+++ Truniak O/TrOlha_Ch. 8, Ex. 12.py
@@ -4,11 +4,19 @@
 # We have seen that a chart parser adds but never removes edges from a chart. Why?
 
 
-print """ Notice that we have not used any built-in parsing functions here. We’ve implemented
-a complete primitive chart parser from the ground up! The chart data structure: Non-terminals
-are represented as extra edges in the chart. WFSTs have several shortcomings. First, as you can
-see, the WFST is not itself a parse tree, so the technique is strictly speaking recognizing
-that a sentence is admitted by agrammar, rather than parsing it. Second, it requires every
-non-lexical grammar production to be binary. Although it is possible to convert an arbitrary
-CFG into this form, we would prefer to use an approach without such a requirement. Chart parsers
-use a slightly richer data structure and some interesting algorithms to solve these problems"""
+print """ Chart parsers use a set of rules to heuristically decide when an edge
+should be added to a chart. This set of rules, along with a specification of when
+they should be applied, forms a strategy. One rule is particularly important, since 
+it is used by every chart parser: the Fundamental Rule. This rule is used to combine
+an incomplete edge that's expecting a nonterminal B with a following, complete edge 
+whose left hand side is B. 
+Fundamental Rule
+If the chart contains the edges
+  [A → α • B β , (i, j)]
+  [B → γ • , (j, k)]
+then add the new edge
+  [A → α B • β , (i, k)]
+where α, β, and γ are (possibly empty) sequences of terminals or non-terminals.
+Note that the dot has moved one place to the right, and the span of this new edge is 
+the combined span of the other two. Note also that in adding this new edge we do not 
+remove the other two, because they might be used again."""
