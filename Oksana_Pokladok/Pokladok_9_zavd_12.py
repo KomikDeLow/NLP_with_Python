56726df8067c7a0bd1e40550f970a7fbf672d7bd
Oksana <1blackrose@ukr.net>
2013-05-25 21:48:30 +0300

    added 9.12

---
+++ Oksana_Pokladok/Pokladok_9_zavd_12.py
@@ -0,0 +1,9 @@
+#Pokladok Oksana
+
+S -> NP VP
+NP -> Det N
+VP -> V Det N P N | V N P Det N 
+Det -> 'The'| 'the'
+N -> 'farmer' | 'cart' | 'sand' 
+V -> 'loaded' | 'filled' | 'dumped'
+P -> 'into' | 'with'
