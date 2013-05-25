a67dc3d3ff52ae265fd4204e9bc21d6902844e66
Oksana <1blackrose@ukr.net>
2013-05-25 20:29:19 +0300

    added 8.27

---
+++ Oksana_Pokladok/Pokladok_8_zavd_27.py
@@ -0,0 +1,7 @@
+#Oksana Pokladok
+#In this algorithm, we write the position of words in a triangular matrix which has the form:
+#|a11  a12	a13| 
+#|0	  a22	a23|
+#|0	  0	  a33|
+#Vertical axis indicates the starting position of the matrix, and the horizontal axis end position.
+# To fill this matrix, we need to multiply the length of the input sentence 3.
