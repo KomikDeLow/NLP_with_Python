3f4f0f7161c533722788c892146387cb20120bf9
zakala <marija.zakala@gmail.com>
2013-05-25 23:25:00 +0400

    Update 6chapter. 4 ex.py

--- 6chapter. 4 ex.py
+++ 6chapter. 4 ex.py
@@ -1,21 +1,26 @@
-# TODO
-# bad
-# it isn't the Python modul
-
->>> import nltk
->>>  from nltk.corpus import movie_reviews
->>> documents = [(list(movie_reviews.words(fileid)), category)
-       for category in movie_reviews.fileid.categories()
+import random
+import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import movie_reviews
+stop = stopwords.words('english') + list(",.-!?`':;()")
+documents = [(list(movie_reviews.words(fileid)), category)
+	     for category in movie_reviews.categories()
 	     for fileid in movie_reviews.fileids(category)]
->>> random.shuffle(documents)
->>> all_words = nltk.FreqDist(w.lower() for w in movie_reviews.words())
->>> word_features = all_words.keys() [:2000]
->>> def document_features(document):
+
+random.shuffle(documents)
+words = nltk.FreqDist(w.lower() for w in movie_reviews.words()
+		      if not w in stop)
+
+word_features = words.keys()[:300]
+def document_features(document):
 	document_words = set(document)
-	features ={}
+	features = {}
 	for word in word_features:
-		features['contains(%s)' % word] = (word in document_words)
+		features[word] = (word in document_words)
 	return features
->>> featuresets = [(document_features(d),c) for (d,c) in documents]
->>> classifier = nltk.NaiveBayesClassifier.train(train_set)
->>> Classifier.show_most_informative_features(30)
+
+featuresets = [(document_features(d), c) for (d,c) in documents]
+train_set, test_set = featuresets[400:], featuresets[:400]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print nltk.classify.accuracy(classifier, test_set)
+classifier.show_most_informative_features(30)
