d3d997294a431d102164b0dd413085253dbdec58
Pavutynka <marichka.pelekh@gmail.com>
2013-05-25 23:55:03 +0300

    5_13

---
+++ PM_5_13.py
@@ -0,0 +1,11 @@
+import datetime
+today = datetime.datetime.now()
+# display today's date
+print today.strftime("Today is: %Y-%m-%d")
+day = today.day
+month = today.month
+year = today.year
+day
+25
+
+                     
