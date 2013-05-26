570fe2e6617d714eaf07ec16a968d4354cc3ec45
Valera Androshchuk <pe4ene4ka@gmail.com>
2013-05-26 03:15:12 +0300

    Ch10_Task2

---
+++ Valerii_Androshchuk/10-2.py
@@ -0,0 +1,30 @@
+# Iuliia Tsymbalista APL(s)-13
+# chapter 10, Task 2
+# Translate the following sentences into
+# predicate-argument formulas of first-order logic.
+
+# a)Angus likes Cyril and Irene hates Cyril.
+# 'like(Angus, Cyril) and hate(Irene, Cyril)'
+# 'like(x,y) and hate (z,y)
+
+# b)Tofu is taller than Bertie.
+# 'taller(Tofu, Bertie)'
+# 'taller(x,y)'
+
+# c)Bruce loves himself and Pat does too.
+# 'love(Bruce, Bruce) and love (Pat, Bruce)'
+# 'love(x,x) and love(y,x)'
+
+# d)Cyril saw Bertie, but Angus didn’t.
+# 'saw(Cyril, Bertie) and -saw(Angus, Bertie)'
+# 'saw(x,y) and -saw(z,y)'
+
+# e)Cyril is a four-legged friend.
+# 'be_friend(Cyril)'
+# 'be_friend(x)'
+
+# f)Tofu and Olive are near each other.
+# 'is_near(Tofu, Olive) and is_near(Olive, Tofu)'
+# 'is_near(x,y) and is_near(y,x)'
+
+# Should it be somehow displayed after using "Run module" function?
