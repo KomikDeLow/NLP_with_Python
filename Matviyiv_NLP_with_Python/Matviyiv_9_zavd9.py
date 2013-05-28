c51165cecb364d70cc0013c049a5d2475f7fc4ac
mriynuk <mriynuk99@gmail.com>
2013-05-28 12:29:00 +0300

    error corrected

--- Matviyiv_NLP_with_Python/Matviyiv_9_zavd9.py
+++ Matviyiv_NLP_with_Python/Matviyiv_9_zavd9.py
@@ -9,5 +9,5 @@ main_fs=nltk.FeatStruct(A='X',B='X')# Main Feture set
 fsA=nltk.FeatStruct(A='X')# subsume struxtures
 fsB=nltk.FeatStruct(B='X')
 
-print 'fsA subsumes main_fs: ',fs1.subsumes(main_fs)# cheking if the builded Structures subsume the main structure
-print 'fsB subsumes main_fs: ', fs2.subsumes(main_fs)
+print 'fsA subsumes main_fs: ',fsA.subsumes(main_fs)# cheking if the builded Structures subsume the main structure
+print 'fsB subsumes main_fs: ', fsB.subsumes(main_fs)
