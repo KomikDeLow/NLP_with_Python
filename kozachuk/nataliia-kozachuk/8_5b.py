51afe444d881aa4553b70fa9f11c08662669d764
Nataliia Kozachuk <natalyakozatchuk@yahoo.com>
2013-05-28 14:18:51 +0300

    8_5b

---
+++ kozachuk/nataliia-kozachuk/8_5b.py
@@ -0,0 +1,11 @@
+#8_5b
+import nltk
+t1=nltk.Tree('(S(NP I)(VP(VP(V shot) (NP(Det an) (N elefant))) (PP((P in) (NP((Det my) (N pajamas)))))))')
+t1.draw()
+t2=nltk.Tree('(S((NP((Det the)(Nom((Adj little)(N bear)))))(VP((VP(V saw)(NP(Det the)(Nom(Adj fine)(Adj fat)(N trout))))(PP(P in)(NP(Det the)(Nom(N brook))))))))')
+t2.draw()
+t3=nltk.Tree('(S((NP(Det the)(Nom((Adj angry)(Nom(N bear)))))(VP(V chased)(NP((Det the)(Nom((Adj little)(N squirrell))))))))')
+t3.draw()
+t4=nltk.Tree('(S((NP(PropN Chattere))(VP(V said)(S(NP(PropN Buster))(VP(V thought)(S((NP((Det the)(N tree)))(VP(V was)(Adj tall)))))))))')
+
+t4.draw()
