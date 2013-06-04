0e14e10a22f6532906112be2d33d19a38ea48626
Andriy Romanyuk <anrom7@gmail.com>
2013-06-04 13:23:10 +0300

    12

--- Vezdenko_Chapter5_Ex32.py
+++
@@ -1,6 +0,0 @@
-#Vezdenko##
-import nltk
-help(nltk.tag.brill.demo)
-nltk.tag.brill.demo(num_sents=90, max_rules=100, min_score=4, trace=7, train=0.9)
-nltk.tag.brill.demo(num_sents=1000, max_rules=100, min_score=4, trace=7, train=0.8)
-nltk.tag.brill.demo(num_sents=500, max_rules=100, min_score=4, trace=7, train=1)
--- Vezdenko_Chapter7_Ex1.py
+++
@@ -1,2 +0,0 @@
-#Vezdenko##
-IOB format are nacessary because it provides mothods for readingand writting trees in Python. If we use only I and O, it will caused problem bacause Tag B marks the beginning of the chunk. so we could have errors.
