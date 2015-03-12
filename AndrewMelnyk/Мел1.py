5cb82850fe0a87f2606fd1481355289c7d94b462
Andriy Romanyuk <anrom7@gmail.com>
2015-03-12 09:15:08 +0200

    add new file for tassk #23

---
+++ AndrewMelnyk/Мел1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# vgdghjgjg  tht
+d='kbj'
+for i in d:
+     print (i)
+
+b=['fdb','sdfb','sbgf','sfdbg','sfdg']
